ab0e91328deacafa52d268ea7015365561481e9a
Peter Ansell <p_ansell@yahoo.com>
2012-09-11 15:55:52 +1000

    abstract out main routines from skeleton test so they are reusable

--- podd-prototype/src/test/java/com/github/podd/prototype/test/PoddPrototypeSkeletonTest.java
+++ podd-prototype/src/test/java/com/github/podd/prototype/test/PoddPrototypeSkeletonTest.java
@@ -3,7 +3,7 @@
  */
 package com.github.podd.prototype.test;
 
-import static org.junit.Assert.*;
+import java.io.IOException;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -11,12 +11,16 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openrdf.model.Statement;
 import org.openrdf.model.URI;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.RepositoryException;
 import org.openrdf.repository.util.RDFInserter;
 import org.semanticweb.owlapi.formats.RDFXMLOntologyFormatFactory;
 import org.semanticweb.owlapi.io.StreamDocumentSource;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyChangeException;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyID;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.model.OWLOntologyManagerFactoryRegistry;
@@ -24,10 +28,13 @@ import org.semanticweb.owlapi.model.SetOntologyID;
 import org.semanticweb.owlapi.profiles.OWLProfile;
 import org.semanticweb.owlapi.profiles.OWLProfileRegistry;
 import org.semanticweb.owlapi.profiles.OWLProfileReport;
+import org.semanticweb.owlapi.reasoner.InconsistentOntologyException;
 import org.semanticweb.owlapi.reasoner.InferenceType;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
 import org.semanticweb.owlapi.reasoner.OWLReasonerFactoryRegistry;
+import org.semanticweb.owlapi.reasoner.ReasonerInterruptedException;
+import org.semanticweb.owlapi.reasoner.TimeOutException;
 import org.semanticweb.owlapi.rio.RioRenderer;
 import org.semanticweb.owlapi.util.InferredOntologyGenerator;
 
@@ -49,7 +56,87 @@ public class PoddPrototypeSkeletonTest extends AbstractSesameTest
     private URI testContextUri;
     private URI testInferredContextUri;
     private OWLReasoner reasoner;
+    private OWLReasonerFactory reasonerFactory;
+    private String reasonerName;
+    private OWLOntology inferredAxiomsOntology;
     
+    protected OWLReasoner checkConsistency(final OWLOntology nextOntology, final IRI nextProfileIRI) throws Exception
+    {
+        final OWLProfile nextProfile = OWLProfileRegistry.getInstance().getProfile(nextProfileIRI);
+        Assert.assertNotNull("Could not find profile in registry", nextProfile);
+        final OWLProfileReport profileReport = nextProfile.checkOntology(nextOntology);
+        Assert.assertTrue("Schema Ontology was not in the given profile", profileReport.isInProfile());
+        
+        // create an OWL Reasoner using the Pellet library and ensure that the reasoner thinks the
+        // ontology is consistent so far
+        // Use the factory that we found to create a reasoner over the ontology
+        final OWLReasoner nextReasoner = this.reasonerFactory.createReasoner(nextOntology);
+        
+        // Test that the ontology was consistent with this reasoner
+        // This ensures in the case of Pellet that it is in the OWL2-DL profile
+        Assert.assertTrue("Ontology was not consistent", nextReasoner.isConsistent());
+        
+        return nextReasoner;
+    }
+    
+    /**
+     * @throws ReasonerInterruptedException
+     * @throws TimeOutException
+     * @throws InconsistentOntologyException
+     * @throws OWLOntologyCreationException
+     * @throws OWLOntologyChangeException
+     */
+    protected OWLOntology computeInferences(final OWLReasoner nextReasoner, final IRI inferredOntologyUri,
+            final IRI inferredOntologyVersionUri) throws ReasonerInterruptedException, TimeOutException,
+        InconsistentOntologyException, OWLOntologyCreationException, OWLOntologyChangeException
+    {
+        nextReasoner.precomputeInferences(InferenceType.CLASS_HIERARCHY);
+        final InferredOntologyGenerator iog = new InferredOntologyGenerator(nextReasoner);
+        final OWLOntology nextInferredAxiomsOntology =
+                this.manager.createOntology(new OWLOntologyID(inferredOntologyUri, inferredOntologyVersionUri));
+        iog.fillOntology(this.manager, nextInferredAxiomsOntology);
+        
+        return nextInferredAxiomsOntology;
+    }
+    
+    /**
+     * @param nextContextUri
+     *            The context URI to dump the ontology triples into.
+     * @param nextOntology
+     *            The ontology to dump into the repository.
+     * @param nextRepositoryConnection
+     *            The repository connection to dump the triples into.
+     * @throws IOException
+     * @throws RepositoryException
+     */
+    protected void dumpOntologyToRepository(final URI nextContextUri, final OWLOntology nextOntology,
+            final RepositoryConnection nextRepositoryConnection) throws IOException, RepositoryException
+    {
+        // Create an RDFHandler that will insert all triples after they are emitted from OWLAPI into
+        // a single context in the Sesame Repository
+        final RDFInserter repositoryHandler = new RDFInserter(nextRepositoryConnection);
+        repositoryHandler.enforceContext(nextContextUri);
+        
+        // Render the triples out from OWLAPI into a Sesame Repository
+        final RioRenderer renderer =
+                new RioRenderer(nextOntology, this.manager, repositoryHandler, null, nextContextUri);
+        renderer.render();
+        
+        // Commit the current repository connection
+        nextRepositoryConnection.commit();
+    }
+    
+    protected OWLOntology loadOntology(final String ontologyResource) throws Exception
+    {
+        final OWLOntology nextOntology =
+                this.manager.loadOntologyFromOntologyDocument(new StreamDocumentSource(this.getClass()
+                        .getResourceAsStream(ontologyResource), new RDFXMLOntologyFormatFactory()));
+        Assert.assertFalse(nextOntology.isEmpty());
+        
+        return nextOntology;
+    }
+    
+    @Override
     @Before
     public void setUp() throws Exception
     {
@@ -58,8 +145,13 @@ public class PoddPrototypeSkeletonTest extends AbstractSesameTest
         // create the manager to use for the test
         this.manager = OWLOntologyManagerFactoryRegistry.createOWLOntologyManager();
         
+        // TODO: Pellet should be configurable
+        this.reasonerName = "Pellet";
+        this.reasonerFactory = OWLReasonerFactoryRegistry.getInstance().getReasonerFactory(this.reasonerName);
+        Assert.assertNotNull("Could not find reasoner", this.reasonerFactory);
     }
     
+    @Override
     @After
     public void tearDown() throws Exception
     {
@@ -71,6 +163,13 @@ public class PoddPrototypeSkeletonTest extends AbstractSesameTest
             this.reasoner.dispose();
             this.reasoner = null;
         }
+    }
+    
+    @Test
+    public final void testBaseOntology() throws Exception
+    {
+        this.testOntology = this.loadOntology("/ontologies/poddBase.owl");
+        this.reasoner = this.checkConsistency(this.testOntology, OWLProfile.OWL2_DL);
     }
     
     @Test
@@ -87,39 +186,16 @@ public class PoddPrototypeSkeletonTest extends AbstractSesameTest
         this.testContextUri = this.getTestValueFactory().createURI("urn:test:plantontology:context");
         this.testInferredContextUri = this.getTestValueFactory().createURI("urn:test:plantontology:inferred:context");
         
-        // load the ontology from an RDF/XML file
-        // TODO: Replace this with the PODD ontologies after verifying them
-        this.testOntology =
-                this.manager.loadOntologyFromOntologyDocument(new StreamDocumentSource(this.getClass()
-                        .getResourceAsStream("/test/ontologies/plant_ontology-v16.owl"),
-                        new RDFXMLOntologyFormatFactory()));
-        Assert.assertFalse(this.testOntology.isEmpty());
+        this.testOntology = this.loadOntology("/test/ontologies/plant_ontology-v16.owl");
         
         // fix up the ontology id that was parsed using the values that we wish to use here
-        OWLOntologyID fullId =
+        final OWLOntologyID fullId =
                 new OWLOntologyID(this.testOntology.getOntologyID().getOntologyIRI(),
                         IRI.create("urn:test:plantontology:version:16.0"));
-        this.manager.applyChange(new SetOntologyID(testOntology, fullId));
+        this.manager.applyChange(new SetOntologyID(this.testOntology, fullId));
         Assert.assertEquals(fullId, this.testOntology.getOntologyID());
         
-        // Verify that the ontology is in the OWL2-DL Profile
-        OWLProfile owl2DLProfile = OWLProfileRegistry.getInstance().getProfile(OWLProfile.OWL2_DL);
-        Assert.assertNotNull("Could not find OWL2-DL profile in registry", owl2DLProfile);
-        OWLProfileReport profileReport = owl2DLProfile.checkOntology(testOntology);
-        Assert.assertTrue("Schema Ontology was not in the OWL2-DL Profile", profileReport.isInProfile());
-        
-        // Create an RDFHandler that will insert all triples after they are emitted from OWLAPI into
-        // a single context in the Sesame Repository
-        final RDFInserter repositoryHandler = new RDFInserter(this.getTestRepositoryConnection());
-        repositoryHandler.enforceContext(this.testContextUri);
-        
-        // Render the triples out from OWLAPI into a Sesame Repository
-        final RioRenderer renderer =
-                new RioRenderer(this.testOntology, this.manager, repositoryHandler, null, this.testContextUri);
-        renderer.render();
-        
-        // Commit the current repository connection
-        this.getTestRepositoryConnection().commit();
+        this.dumpOntologyToRepository(this.testContextUri, this.testOntology, this.getTestRepositoryConnection());
         
         // Then test to ensure that the expected number of triples were added,
         // and ensure that no other triples are in the repository at this point
@@ -128,38 +204,17 @@ public class PoddPrototypeSkeletonTest extends AbstractSesameTest
         
         // create an OWL Reasoner using the Pellet library and ensure that the reasoner thinks the
         // ontology is consistent so far
-        // TODO: Pellet should be configurable
-        final String reasonerName = "Pellet";
-        final OWLReasonerFactory reasonerFactory =
-                OWLReasonerFactoryRegistry.getInstance().getReasonerFactory(reasonerName);
-        Assert.assertNotNull("Could not find reasoner", reasonerFactory);
-        
-        // Use the factory that we found to create a reasoner over the ontology
-        this.reasoner = reasonerFactory.createReasoner(this.testOntology);
-        
-        // Test that the ontology was consistent with this reasoner
-        // This ensures in the case of Pellet that it is in the OWL2-DL profile
-        Assert.assertTrue("Ontology was not consistent", this.reasoner.isConsistent());
+        this.reasoner = this.checkConsistency(this.testOntology, OWLProfile.OWL2_DL);
         
         // use the reasoner to infer concrete axioms and deposit them in a new ontology
-        this.reasoner.precomputeInferences(InferenceType.CLASS_HIERARCHY);
-        final InferredOntologyGenerator iog = new InferredOntologyGenerator(this.reasoner);
-        final OWLOntology inferredAxiomsOntology =
-                this.manager.createOntology(new OWLOntologyID(IRI.create(this.testInferredContextUri), IRI
-                        .create("urn:test:inferredontology:plantontology:version:16.0")));
-        iog.fillOntology(this.manager, inferredAxiomsOntology);
+        this.inferredAxiomsOntology =
+                this.computeInferences(this.reasoner, IRI.create(this.testInferredContextUri),
+                        IRI.create("urn:test:inferredontology:plantontology:version:16.0"));
         
         // Dump the triples from the inferred axioms into a separate SPARQL Graph/Context in the
         // Sesame Repository
-        final RDFInserter inferredRepositoryHandler = new RDFInserter(this.getTestRepositoryConnection());
-        inferredRepositoryHandler.enforceContext(this.testInferredContextUri);
-        
-        final RioRenderer inferencesRenderer =
-                new RioRenderer(inferredAxiomsOntology, this.manager, inferredRepositoryHandler, null,
-                        this.testInferredContextUri);
-        inferencesRenderer.render();
-        
-        this.getTestRepositoryConnection().commit();
+        this.dumpOntologyToRepository(this.testInferredContextUri, this.inferredAxiomsOntology,
+                this.getTestRepositoryConnection());
         
         // verify that the triples were inserted into the repository correctly
         Assert.assertEquals(2995, this.getTestRepositoryConnection().size(this.testInferredContextUri));
@@ -177,10 +232,10 @@ public class PoddPrototypeSkeletonTest extends AbstractSesameTest
         
         // TODO:
         
-        OWLOntology updatedSchemaOntology =
+        final OWLOntology updatedSchemaOntology =
                 this.manager.createOntology(new OWLOntologyID(this.testOntology.getOntologyID().getOntologyIRI(), IRI
                         .create("urn:test:plantontology:version:16.1")));
-        this.manager.addAxioms(updatedSchemaOntology, testOntology.getAxioms());
+        this.manager.addAxioms(updatedSchemaOntology, this.testOntology.getAxioms());
         
         // Load a set of objects in as an ontology that imports the plant ontology into the system
         // and verify that it is consistent
