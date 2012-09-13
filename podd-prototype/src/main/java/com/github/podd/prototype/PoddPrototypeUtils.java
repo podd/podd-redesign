c65de1fc3f64df8b5ae86f61ed4fc79959fddb32
Peter Ansell <p_ansell@yahoo.com>
2012-09-13 15:38:12 +1000

    javadoc for PoddPrototypeUtil

--- podd-prototype/src/main/java/com/github/podd/prototype/PoddPrototypeUtils.java
+++ podd-prototype/src/main/java/com/github/podd/prototype/PoddPrototypeUtils.java
@@ -6,6 +6,7 @@ package com.github.podd.prototype;
 import java.io.IOException;
 
 import org.junit.Assert;
+import org.openrdf.model.Resource;
 import org.openrdf.model.URI;
 import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.RepositoryException;
@@ -27,6 +28,7 @@ import org.semanticweb.owlapi.reasoner.OWLReasoner;
 import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
 import org.semanticweb.owlapi.reasoner.ReasonerInterruptedException;
 import org.semanticweb.owlapi.reasoner.TimeOutException;
+import org.semanticweb.owlapi.rio.RioMemoryTripleSource;
 import org.semanticweb.owlapi.rio.RioRenderer;
 import org.semanticweb.owlapi.util.InferredOntologyGenerator;
 import org.slf4j.Logger;
@@ -50,6 +52,20 @@ public class PoddPrototypeUtils
         // TODO Auto-generated constructor stub
     }
     
+    /**
+     * Checks the consistency of the ontology and returns the instance of OWLReasoner that was used
+     * to check the consistency.
+     * 
+     * @param nextOntology
+     *            The ontology to check for consistency.
+     * @param nextProfileIRI
+     *            The IRI of the OWL Profile to use for checking consistency.
+     * @param nextReasonerFactory
+     *            The OWLReasonerFactory that is able to create instances of an OWLReasoner that are
+     *            compatible with the given OWL Profile.
+     * @return An instance of OWLreasoner that was used to check the consistency.s
+     * @throws Exception
+     */
     public OWLReasoner checkConsistency(final OWLOntology nextOntology, final IRI nextProfileIRI,
             final OWLReasonerFactory nextReasonerFactory) throws Exception
     {
@@ -78,8 +94,20 @@ public class PoddPrototypeUtils
     }
     
     /**
+     * Computes the inferences using the given reasoner, which has previously been setup based on an
+     * ontology.
+     * 
+     * @param nextReasoner
+     *            The reasoner to use to compute the inferences.
+     * @param inferredOntologyUri
+     *            The IRI to use for the resulting OWLOntology containing the inferred statements.
+     * @param inferredOntologyVersionUri
+     *            The IRI to use for the version of the resulting OWLOntology containing the
+     *            inferred statements.
      * @param ontologyManager
-     *            TODO
+     *            The OWLOntologyManager to use for the process.
+     * @return An OWLOntology instance containing the axioms that were inferred from the original
+     *         ontology.
      * @throws ReasonerInterruptedException
      * @throws TimeOutException
      * @throws InconsistentOntologyException
@@ -101,6 +129,8 @@ public class PoddPrototypeUtils
     }
     
     /**
+     * Dump the triples representing a given ontology into a Sesame Repository.
+     * 
      * @param nextContextUri
      *            The context URI to dump the ontology triples into.
      * @param nextOntology
@@ -108,7 +138,7 @@ public class PoddPrototypeUtils
      * @param nextRepositoryConnection
      *            The repository connection to dump the triples into.
      * @param ontologyManager
-     *            TODO
+     *            The ontology manager containing the given ontology.
      * @throws IOException
      * @throws RepositoryException
      */
@@ -138,6 +168,44 @@ public class PoddPrototypeUtils
         }
     }
     
+    /**
+     * Loads an ontology from a Sesame RepositoryConnection, given an optional set of contexts.
+     * 
+     * @param conn
+     *            The Sesame RepositoryConnection object to use when loading the ontology.
+     * @param ontologyManager
+     *            The ontology manager to use when loading the ontology.
+     * @param contexts
+     *            An optional varargs array of contexts specifying the contexts to use when loading
+     *            the ontology. If this is missing the entire repository will be used.
+     * @return An OWLOntology instance populated with the triples from the repository.
+     * @throws Exception
+     */
+    public OWLOntology loadOntology(final RepositoryConnection conn, final OWLOntologyManager ontologyManager,
+            final Resource... contexts) throws Exception
+    {
+        final RioMemoryTripleSource tripleSource =
+                new RioMemoryTripleSource(conn.getStatements(null, null, null, true, contexts));
+        tripleSource.setNamespaces(conn.getNamespaces());
+        
+        final OWLOntology nextOntology = ontologyManager.loadOntologyFromOntologyDocument(tripleSource);
+        
+        Assert.assertFalse(nextOntology.isEmpty());
+        
+        return nextOntology;
+    }
+    
+    /**
+     * Loads an ontology from a Java Resource on the classpath. This is useful for loading test
+     * resources.
+     * 
+     * @param ontologyResource
+     *            The classpath location of the test resource to load.
+     * @param ontologyManager
+     *            The ontology manager to use when loading the ontology.
+     * @return An OWLOntology instance populated with the triples from the classpath resource.
+     * @throws Exception
+     */
     public OWLOntology loadOntology(final String ontologyResource, final OWLOntologyManager ontologyManager)
         throws Exception
     {
