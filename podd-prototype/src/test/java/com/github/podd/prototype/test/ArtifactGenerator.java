9b422cec95ffcc9884f6582ce4c39f624cc3fc90
Kutila Gunasekera <kutila@gmail.com>
2012-10-19 15:35:45 +1000

    Refactored to generate more similar ontologies for both plant and science

--- podd-prototype/src/test/java/com/github/podd/prototype/test/ArtifactGenerator.java
+++ podd-prototype/src/test/java/com/github/podd/prototype/test/ArtifactGenerator.java
@@ -16,7 +16,6 @@ import org.openrdf.model.vocabulary.XMLSchema;
 import org.openrdf.repository.Repository;
 import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.RepositoryException;
-import org.openrdf.repository.RepositoryResult;
 import org.openrdf.repository.sail.SailRepository;
 import org.openrdf.rio.RDFFormat;
 import org.openrdf.rio.RDFHandler;
@@ -33,6 +32,7 @@ import org.semanticweb.owlapi.model.IRI;
 public class ArtifactGenerator
 {
     
+    private static final String PLANT_PREFIX = "http://purl.obolibrary.org/obo/";
     private static final String PODD_SCIENCE = "http://purl.org/podd/ns/poddScience#";
     private static final String PODD_BASE = "http://purl.org/podd/ns/poddBase#";
     
@@ -40,39 +40,28 @@ public class ArtifactGenerator
     private static final String poddSciencePath = "/ontologies/poddScience.owl";
     private static final String poddAnimalPath = "/ontologies/poddAnimal.owl";
     
+    private static final String filePath =
+            "/home/kutila/gitrepos/podd-redesign/podd-prototype/src/test/resources/test/artifacts/";
+    
     private static final URI contains = IRI.create(ArtifactGenerator.PODD_BASE, "contains").toOpenRDFURI();
+    private static final URI containedBy = IRI.create(ArtifactGenerator.PODD_BASE, "containedBy").toOpenRDFURI();
+    
+    private static final URI adjacentTo = IRI.create(ArtifactGenerator.PLANT_PREFIX, "po#adjacent_to").toOpenRDFURI();
+    
+    private final RepositoryConnection repositoryConnection;
+    private final ValueFactory valueFactory;
     
     /**
      * @param args
      */
     public static void main(final String[] args) throws Exception
     {
-        final String filePath = "/home/kutila/gitrepos/podd-redesign/podd-prototype/src/test/resources/test/artifacts/";
+        final String filePrefix = "science-";
+        final int maxResources = 3000;
+        final boolean isDeep = true; // only for Science
+        final boolean isPlant = false;
+        final int i = 4;
         
-        final ArtifactGenerator generator = new ArtifactGenerator();
-        
-        final String filePrefix = "largeProject-";
-        for(int i = 5; i < 6; i++)
-        {
-            generator.createPoddArtifact(filePath, filePrefix, i);
-            System.out.println("Created " + filePrefix + i + ".rdf");
-        }
-    }
-    
-    /**
-     * Create a PODD artifact object and saves it in RDF/XML format.
-     * 
-     * @param filePath
-     *            Location where file should be created
-     * @param filePrefix
-     *            File name prefix
-     * @param index
-     *            Numeric end to the file name
-     * @throws Exception
-     */
-    public final void createPoddArtifact(final String filePath, final String filePrefix, final int index)
-        throws Exception
-    {
         // create repository
         final Repository inMemoryRepository = new SailRepository(new MemoryStore());
         inMemoryRepository.initialize();
@@ -80,44 +69,126 @@ public class ArtifactGenerator
         nextRepositoryConnection.setAutoCommit(false);
         final ValueFactory valueFac = inMemoryRepository.getValueFactory();
         
-        // set namespaces
-        nextRepositoryConnection.setNamespace(OWL.PREFIX, OWL.NAMESPACE);
-        nextRepositoryConnection.setNamespace(RDFS.PREFIX, RDFS.NAMESPACE);
-        nextRepositoryConnection.setNamespace("poddBase", ArtifactGenerator.PODD_BASE);
-        nextRepositoryConnection.setNamespace("poddScience", ArtifactGenerator.PODD_SCIENCE);
+        // generate artifact and write to file
+        final ArtifactGenerator generator = new ArtifactGenerator(nextRepositoryConnection, valueFac);
+        generator.createArtifact(isPlant, filePrefix, maxResources, i, isDeep);
         
-        // add ontology (i.e. artifact) object
-        final URI artifactURI = valueFac.createURI("urn:temp:artifact:" + index);
-        final URI versionIRI = valueFac.createURI("urn:temp:artifact:version:1");
-        final URI owlVersionIRI = valueFac.createURI(OWL.NAMESPACE, "versionIRI");
-        nextRepositoryConnection.add(artifactURI, RDF.TYPE, OWL.ONTOLOGY);
-        nextRepositoryConnection.add(artifactURI, owlVersionIRI, versionIRI);
+        // clean up
+        nextRepositoryConnection.close();
+        inMemoryRepository.shutDown();
+    }
+    
+    public ArtifactGenerator(final RepositoryConnection nextRepositoryConnection, final ValueFactory nextValueFactory)
+    {
+        this.repositoryConnection = nextRepositoryConnection;
+        this.valueFactory = nextValueFactory;
+    }
+    
+    public final void createArtifact(final boolean isPlant, final String filePrefix, final int maxResources,
+            final int index, final boolean isDeep) throws Exception
+    {
+        // set the namespaces
+        this.repositoryConnection.setNamespace(OWL.PREFIX, OWL.NAMESPACE);
+        this.repositoryConnection.setNamespace(RDFS.PREFIX, RDFS.NAMESPACE);
         
-        // import PoddBase and PoddScience
-        nextRepositoryConnection.add(artifactURI, OWL.IMPORTS, valueFac.createURI("http://purl.org/podd/ns/poddBase"));
-        nextRepositoryConnection.add(artifactURI, OWL.IMPORTS,
-                valueFac.createURI("http://purl.org/podd/ns/poddScience"));
+        // create ontology (i.e. artifact)
+        final URI artifactURI = this.valueFactory.createURI("urn:temp:artifact:" + index);
+        final URI versionIRI = this.valueFactory.createURI("urn:temp:artifact:version:1");
+        this.repositoryConnection.add(artifactURI, RDF.TYPE, OWL.ONTOLOGY);
         
-        // add a Project as Top Object
-        final URI projectURI = this.addPoddProject(index, nextRepositoryConnection, valueFac, artifactURI);
+        final URI owlVersionIRI = this.valueFactory.createURI(OWL.NAMESPACE, "versionIRI");
+        this.repositoryConnection.add(artifactURI, owlVersionIRI, versionIRI);
         
-        final List<URI> resources = this.addPoddInternalObjects(nextRepositoryConnection, valueFac, 200, projectURI);
-        nextRepositoryConnection.commit();
-        
-        // for(final URI u : resources)
-        // {
-        // nextRepositoryConnection.add(projectURI, contains, u);
-        // }
-        nextRepositoryConnection.commit();
-        
-        // this.printGraph(nextRepositoryConnection);
+        long objectCount = 0;
+        if(isPlant)
+        {
+            objectCount = this.createPlantArtifact(maxResources, index, artifactURI);
+        }
+        else
+        // science
+        {
+            // additional namespaces
+            this.repositoryConnection.setNamespace("poddBase", ArtifactGenerator.PODD_BASE);
+            this.repositoryConnection.setNamespace("poddScience", ArtifactGenerator.PODD_SCIENCE);
+            
+            // import PoddBase and PoddScience
+            this.repositoryConnection.add(artifactURI, OWL.IMPORTS,
+                    this.valueFactory.createURI("http://purl.org/podd/ns/poddBase"));
+            this.repositoryConnection.add(artifactURI, OWL.IMPORTS,
+                    this.valueFactory.createURI("http://purl.org/podd/ns/poddScience"));
+            
+            objectCount = this.createScienceArtifact(artifactURI, maxResources, isDeep, index);
+        }
         
         // dump the RDF to a file
-        final FileOutputStream fos = new FileOutputStream(filePath + filePrefix + index + ".rdf");
+        final FileOutputStream fos = new FileOutputStream(ArtifactGenerator.filePath + filePrefix + index + ".rdf");
         final RDFHandler rdfxmlWriter = new RDFXMLWriter(fos);
-        nextRepositoryConnection.export(rdfxmlWriter);
-        nextRepositoryConnection.commit();
-        nextRepositoryConnection.close();
+        this.repositoryConnection.export(rdfxmlWriter);
+        this.repositoryConnection.commit();
+        
+        System.out.println("Created " + filePrefix + index + ".rdf");
+        System.out.println("  No. of triples = " + this.repositoryConnection.size());
+        System.out.println("  No. of objects = " + objectCount);
+    }
+    
+    private long createPlantArtifact(final int maxResources, final int index, final URI artifactURI)
+        throws RepositoryException
+    {
+        // import Plant ontology
+        final URI plantOntology = this.valueFactory.createURI(ArtifactGenerator.PLANT_PREFIX + "po.owl");
+        this.repositoryConnection.add(artifactURI, OWL.IMPORTS, plantOntology);
+        
+        // add objects
+        final URI objPO_0025131 = this.valueFactory.createURI(ArtifactGenerator.PLANT_PREFIX + "PO_0025131");
+        
+        URI previous = objPO_0025131;
+        
+        for(int i = 0; i < maxResources; i++)
+        {
+            final URI individual = this.valueFactory.createURI("urn:temp:PlantAnatomicalEntity:" + i);
+            this.repositoryConnection.add(individual, RDF.TYPE, objPO_0025131);
+            this.repositoryConnection.add(individual, ArtifactGenerator.adjacentTo, previous);
+            this.repositoryConnection.add(individual, RDFS.LABEL,
+                    this.valueFactory.createLiteral("Plant " + index + " anatomical entity " + i));
+            previous = individual;
+        }
+        this.repositoryConnection.commit();
+        return maxResources;
+    }
+    
+    /**
+     * Create a PODD Science artifact object and saves it in RDF/XML format.
+     * 
+     * @param filePath
+     *            Location where file should be created
+     * @param filePrefix
+     *            File name prefix
+     * @param index
+     *            Numeric end to the file name
+     * @return number of objects created
+     * @throws Exception
+     */
+    public final long createScienceArtifact(final URI artifactURI, final int maxResources, final boolean isDeep,
+            final int index) throws Exception
+    {
+        // add a Project as Top Object
+        final URI projectURI = this.addPoddProject(index, this.repositoryConnection, this.valueFactory, artifactURI);
+        
+        final List<URI> resourceList =
+                this.addPoddInternalObjects(this.repositoryConnection, this.valueFactory, 200, projectURI,
+                        maxResources, isDeep);
+        
+        if(!isDeep)
+        {
+            for(final URI resourceURI : resourceList)
+            {
+                this.repositoryConnection.add(projectURI, ArtifactGenerator.contains, resourceURI);
+            }
+        }
+        
+        this.repositoryConnection.commit();
+        
+        return resourceList.size();
     }
     
     /**
@@ -143,7 +214,7 @@ public class ArtifactGenerator
         nextRepositoryConnection.add(topObject, RDF.TYPE, poddProject);
         nextRepositoryConnection.add(topObject, RDF.TYPE, OWL.THING);
         nextRepositoryConnection.add(topObject, RDFS.LABEL,
-                valueFac.createLiteral("Project007 Very very large science artifact"));
+                valueFac.createLiteral("Project007 very large science artifact"));
         final URI hasLeadInstitution = valueFac.createURI(ArtifactGenerator.PODD_BASE + "hasLeadInstitution");
         nextRepositoryConnection.add(topObject, hasLeadInstitution, valueFac.createLiteral("UQ", XMLSchema.STRING));
         
@@ -160,112 +231,56 @@ public class ArtifactGenerator
      * @param valueFac
      * @param repeats
      *            How many objects of each class to create
+     * @param maxResources
+     *            Make up to this number of new resources
      * @return A List containing URIs of all the created objects
      * 
      * @throws Exception
      * @throws RepositoryException
      */
     private List<URI> addPoddInternalObjects(final RepositoryConnection nextRepositoryConnection,
-            final ValueFactory valueFac, final int repeats, final URI projectURI) throws Exception, RepositoryException
+            final ValueFactory valueFac, final int repeats, final URI projectURI, final int maxResources,
+            final boolean isDeep) throws Exception, RepositoryException
     {
         final URI poddInternalObject = valueFac.createURI(ArtifactGenerator.PODD_BASE, "PoddInternalObject");
-        final List<URI> resourceList = new ArrayList<URI>();
         final RepositoryConnection tempRepositoryConnection =
                 ArtifactGenerator.getRepositoryConnection(ArtifactGenerator.poddSciencePath);
-        final RepositoryResult<Statement> allResults =
-                tempRepositoryConnection.getStatements(null, RDFS.SUBCLASSOF, poddInternalObject, false);
         
+        final List<Statement> poddInternalSubTypes =
+                tempRepositoryConnection.getStatements(null, RDFS.SUBCLASSOF, poddInternalObject, false).asList();
+        final int noOfSubTypes = poddInternalSubTypes.size();
         URI parent = projectURI;
-        while(allResults.hasNext())
+        
+        int j = 0;
+        final List<URI> resourceList = new ArrayList<URI>();
+        while(resourceList.size() < maxResources)
         {
-            final Statement stmt = allResults.next();
-            final Resource subj = stmt.getSubject();
-            final String subjStr = subj.stringValue();
-            if(subjStr.lastIndexOf("/") > 0)
+            final Resource subType = poddInternalSubTypes.get(j % noOfSubTypes).getSubject();
+            final String subTypeName = subType.stringValue();
+            if(subTypeName.lastIndexOf("/") > 0)
             {
-                for(int i = 0; i < repeats; i++)
+                final URI randomObj = valueFac.createURI("urn:poddinternal:" + UUID.randomUUID().toString() + ":" + j);
+                resourceList.add(randomObj);
+                nextRepositoryConnection.add(randomObj, RDF.TYPE, subType);
+                nextRepositoryConnection.add(randomObj, RDFS.LABEL,
+                        valueFac.createLiteral(subTypeName.substring(subTypeName.lastIndexOf("/") + 1) + " " + j));
+                if(isDeep)
                 {
-                    final URI randomObj =
-                            valueFac.createURI("urn:poddinternal:" + UUID.randomUUID().toString() + ":" + i);
-                    resourceList.add(randomObj);
-                    nextRepositoryConnection.add(randomObj, RDF.TYPE, subj);
-                    nextRepositoryConnection.add(randomObj, RDFS.LABEL,
-                            valueFac.createLiteral(subjStr.substring(subjStr.lastIndexOf("/") + 1) + " " + i));
-                    nextRepositoryConnection.add(parent, ArtifactGenerator.contains, randomObj);
-                    parent = randomObj;
+                    nextRepositoryConnection.add(randomObj, ArtifactGenerator.containedBy, parent);
+                }
+                parent = randomObj;
+                if(resourceList.size() >= maxResources)
+                {
+                    return resourceList;
                 }
             }
+            j++;
         }
+        
         return resourceList;
     }
     
-    public final void createPlantArtifact(final String filePath, final String filePrefix, final int index)
-        throws Exception
-    {
-        // create repository
-        final Repository inMemoryRepository = new SailRepository(new MemoryStore());
-        inMemoryRepository.initialize();
-        final RepositoryConnection nextRepositoryConnection = inMemoryRepository.getConnection();
-        nextRepositoryConnection.setAutoCommit(false);
-        
-        // set the namespaces
-        nextRepositoryConnection.setNamespace(OWL.PREFIX, OWL.NAMESPACE);
-        nextRepositoryConnection.setNamespace(RDFS.PREFIX, RDFS.NAMESPACE);
-        
-        final ValueFactory valueFac = inMemoryRepository.getValueFactory();
-        
-        // create ontology (i.e. artifact)
-        final URI artifactURI = valueFac.createURI("urn:temp:artifact:" + index);
-        final URI versionIRI = valueFac.createURI("urn:temp:artifact:version:1");
-        nextRepositoryConnection.add(artifactURI, RDF.TYPE, OWL.ONTOLOGY);
-        
-        final URI owlVersionIRI = valueFac.createURI(OWL.NAMESPACE, "versionIRI");
-        nextRepositoryConnection.add(artifactURI, owlVersionIRI, versionIRI);
-        
-        // import Plant ontology
-        final URI plantOntology = valueFac.createURI("http://purl.obolibrary.org/obo/po.owl");
-        nextRepositoryConnection.add(artifactURI, OWL.IMPORTS, plantOntology);
-        
-        // add objects
-        final URI objPO_0025131 = valueFac.createURI("http://purl.obolibrary.org/obo/PO_0025131");
-        final URI objNamedIndividual = valueFac.createURI("http://www.w3.org/2002/07/owl#NamedIndividual");
-        
-        for(int i = 0; i < 40000; i++)
-        {
-            final URI individual = valueFac.createURI("urn:temp:PlantAnatomicalEntity:" + i);
-            nextRepositoryConnection.add(individual, RDF.TYPE, objPO_0025131);
-            nextRepositoryConnection.add(individual, RDF.TYPE, objNamedIndividual);
-            nextRepositoryConnection.add(individual, RDFS.LABEL,
-                    valueFac.createLiteral("Plant " + index + " anatomical entity " + i));
-        }
-        
-        // this.printGraph(nextRepositoryConnection);
-        
-        // dump the RDF to a file
-        final FileOutputStream fos = new FileOutputStream(filePath + filePrefix + index + ".rdf");
-        final RDFHandler rdfxmlWriter = new RDFXMLWriter(fos);
-        nextRepositoryConnection.export(rdfxmlWriter);
-        nextRepositoryConnection.commit();
-        nextRepositoryConnection.close();
-    }
-    
     // ----- helper methods -----
-    public static void printOntology(final String ontologyResource) throws Exception
-    {
-        final RepositoryConnection tempRepositoryConnection =
-                ArtifactGenerator.getRepositoryConnection(ontologyResource);
-        
-        final RepositoryResult<Statement> allStatements =
-                tempRepositoryConnection.getStatements(null, null, null, false);
-        while(allStatements.hasNext())
-        {
-            final Statement stmt = allStatements.next();
-            System.out.print(stmt.getSubject());
-            System.out.print("  " + stmt.getPredicate());
-            System.out.println("  " + stmt.getObject());
-        }
-        allStatements.close();
-    }
     
     public static RepositoryConnection getRepositoryConnection(final String ontologyResource) throws Exception
     {
@@ -282,24 +297,4 @@ public class ArtifactGenerator
         
         return tempRepositoryConnection;
     }
-    
-    private void printGraph(final RepositoryConnection repositoryConnection)
-    {
-        org.openrdf.repository.RepositoryResult<Statement> results;
-        try
-        {
-            results = repositoryConnection.getStatements(null, null, null, false);
-            while(results.hasNext())
-            {
-                final Statement triple = results.next();
-                final String tripleStr = java.net.URLDecoder.decode(triple.toString(), "UTF-8");
-                System.out.println(" --- " + tripleStr);
-            }
-        }
-        catch(final Exception e)
-        {
-            e.printStackTrace();
-        }
-    }
-    
 }
