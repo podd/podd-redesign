05bc906b70d1c29753ccd1545fb2ce09c72b4575
Kutila Gunasekera <kutila@gmail.com>
2012-11-02 14:19:58 +1000

    added more log messages for initialization time

--- podd-prototype/src/main/java/com/github/podd/prototype/PoddServletHelper.java
+++ podd-prototype/src/main/java/com/github/podd/prototype/PoddServletHelper.java
@@ -78,22 +78,27 @@ public class PoddServletHelper
     
     public void setUp(Repository repository) throws RepositoryException
     {
+        this.log.info("... setUp ... valueFactory");
         this.nextRepository = repository;
         this.nextValueFactory = this.nextRepository.getValueFactory();
         
         // create the manager to use for the test
+        this.log.info("... setUp ... OWLOntologyManager");
         this.manager = OWLOntologyManagerFactoryRegistry.createOWLOntologyManager();
         
         // We're only supporting OWL2_DL and Pellet in this prototype
+        this.log.info("... setUp ... ReasonerFactory");
         this.reasonerName = "Pellet";
         this.reasonerFactory = OWLReasonerFactoryRegistry.getInstance().getReasonerFactory(this.reasonerName);
         
         this.pelletOwlProfile = OWLProfile.OWL2_DL;
         
+        this.log.info("... setUp ... graphs");
         this.schemaOntologyManagementGraph =
                 this.nextValueFactory.createURI("urn:test:schemaOntologiesManagementGraph");
         this.poddArtifactManagementGraph = this.nextValueFactory.createURI("urn:test:poddArtifactManagementGraph");
         
+        this.log.info("... setUp PoddPrototypeUtils");
         this.utils =
                 new PoddPrototypeUtils(this.manager, this.pelletOwlProfile, this.reasonerFactory,
                         this.schemaOntologyManagementGraph, this.poddArtifactManagementGraph);
@@ -102,6 +107,7 @@ public class PoddServletHelper
         this.poddSciencePath = "/ontologies/poddScience.owl";
         // this.poddPlantPath = "/ontologies/poddPlant.owl";
         
+        this.log.info("... setUp completed");
     }
     
     /**
