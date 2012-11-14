b5bf0248968a67549052a51f9bb555f0cf1c82c9
Peter Ansell <p_ansell@yahoo.com>
2012-11-14 16:31:44 +1000

    add PoddArtifactManager.updateSchemaImport

--- podd-api/src/main/java/com/github/podd/api/PoddArtifactManager.java
+++ podd-api/src/main/java/com/github/podd/api/PoddArtifactManager.java
@@ -3,11 +3,34 @@
  */
 package com.github.podd.api;
 
+import org.semanticweb.owlapi.model.OWLOntologyID;
+
 /**
- * @author Peter Ansell p_ansell@yahoo.com
+ * Manages PODD Artifacts, including loading and retrieving the ontologies representing the
+ * artifacts and the association of artifacts with schema ontologies.
  * 
+ * @author Peter Ansell p_ansell@yahoo.com
  */
 public interface PoddArtifactManager
 {
+    /**
+     * Updates the importing of the given schema ontology in the given PODD Artifact.
+     * 
+     * The Schema Ontology may have been imported as a different version, and these older versions
+     * must be identified during this process to import the given version of the schema ontology in
+     * place of the old version.
+     * 
+     * If the Schema Ontology was not previously imported by this PODD Artifact, then an import
+     * statement will be added to indicate that the PODD Artifact imports the given version of the
+     * Podd Schema Ontology.
+     * 
+     * @param artifactId
+     *            The Ontology ID for the PODD Artifact which needs to have its Schema Ontology
+     *            imports modified.
+     * @param schemaOntologyId
+     *            The Ontology ID for the Schema Ontology which needs to be added or modified in the
+     *            imports for the PODD Artifact.
+     */
+    void updateSchemaImport(OWLOntologyID artifactId, OWLOntologyID schemaOntologyId);
     
 }
