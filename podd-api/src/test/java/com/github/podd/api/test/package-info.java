887d27584382066c89ca0e41a09c8ed45276bec0
Peter Ansell <p_ansell@yahoo.com>
2012-11-14 10:49:20 +1000

    add test package for abstract api tests

---
+++ podd-api/src/test/java/com/github/podd/api/test/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package containing Abstract tests to verify compliance with the PODD Redesign API interfaces.
+ * 
+ * @author Peter Ansell p_ansell@yahoo.com
+ * 
+ */
+package com.github.podd.api.test;
