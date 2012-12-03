a87669d3cdbee36bd74bcee97b24681508be15e3
Kutila Gunasekera <kutila@gmail.com>
2012-12-04 09:27:56 +1000

    update javadoc with @throws clause to getProcessor()

--- podd-api/src/main/java/com/github/podd/api/PoddProcessorFactory.java
+++ podd-api/src/main/java/com/github/podd/api/PoddProcessorFactory.java
@@ -27,7 +27,7 @@ public interface PoddProcessorFactory<T extends PoddProcessor<I>, I>
      *            The stage that is being queried.
      * @return True if the processors created by this factory can handle the given stage.
      * @throws NullPointerException
-     *            If a NULL value is passed in as the stage.
+     *             If a NULL value is passed in as the stage.
      */
     boolean canHandleStage(PoddProcessorStage stage);
     
@@ -43,13 +43,16 @@ public interface PoddProcessorFactory<T extends PoddProcessor<I>, I>
      * by this method for each call.
      * 
      * @return An instance of PoddProcessor.
+     * @throws RuntimeException
+     *             If an exceptional situation prevents this method from returning a PoddProcessor
+     *             instance.
      */
     T getProcessor();
     
     /**
      * 
      * @return A set of stages that this processor factory is relevant to. An empty set is returned
-     *          if, for some reason this factory does not support any stages.
+     *         if, for some reason this factory does not support any stages.
      */
     Set<PoddProcessorStage> getStages();
 }
