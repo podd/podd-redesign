32d57869b9fe4a9e46f2550c4b4d5dae0a916ce6
Kutila Gunasekera <kutila@gmail.com>
2012-12-05 14:36:14 +1000

    add utility class to API. Contains method to assemble a SPARQL query using an RdfProcessFactory

---
+++ podd-api/src/main/java/com/github/podd/utils/PoddRdfUtils.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package com.github.podd.utils;
+
+import org.openrdf.model.URI;
+
+import com.github.podd.api.PoddRdfProcessor;
+import com.github.podd.api.PoddRdfProcessorFactory;
+
+/**
+ * Utility class containing helper methods for handling RDF content.
+ * 
+ * @author kutila
+ * 
+ */
+public class PoddRdfUtils
+{
+    /**
+     * Builds a SPARQL "construct" query using the given <code>PoddRdfProcessorFactory</code>.
+     * 
+     * @param rdfProcessorFactory
+     *            The Factory from which SPARQL query parts are obtained
+     * @return A SPARQL query String
+     */
+    public static String buildSparqlConstructQuery(
+            final PoddRdfProcessorFactory<? extends PoddRdfProcessor> rdfProcessorFactory)
+    {
+        return PoddRdfUtils.buildSparqlConstructQuery(rdfProcessorFactory, null);
+    }
+    
+    /**
+     * Builds a SPARQL "construct" query using the given <code>PoddRdfProcessorFactory</code> and
+     * Subject URI.
+     * 
+     * @param rdfProcessorFactory
+     *            The Factory from which SPARQL query parts are obtained
+     * @param subject
+     *            A URI of interest that should be used to build the SPARQL query
+     * @return A SPARQL query String
+     */
+    public static String buildSparqlConstructQuery(
+            final PoddRdfProcessorFactory<? extends PoddRdfProcessor> rdfProcessorFactory, final URI subject)
+    {
+        final StringBuilder sparqlBuilder = new StringBuilder();
+        sparqlBuilder.append("CONSTRUCT { ");
+        sparqlBuilder.append(rdfProcessorFactory.getSPARQLConstructBGP());
+        sparqlBuilder.append(" } WHERE { ");
+        
+        if(subject == null)
+        {
+            sparqlBuilder.append(rdfProcessorFactory.getSPARQLConstructWhere());
+        }
+        else
+        {
+            sparqlBuilder.append(rdfProcessorFactory.getSPARQLConstructWhere(subject));
+        }
+        sparqlBuilder.append(" }");
+        if(!rdfProcessorFactory.getSPARQLGroupBy().isEmpty())
+        {
+            sparqlBuilder.append(" GROUP BY ");
+            sparqlBuilder.append(rdfProcessorFactory.getSPARQLGroupBy());
+        }
+        
+        return sparqlBuilder.toString();
+    }
+}
---
+++ podd-api/src/test/java/com/github/podd/utils/test/PoddRdfUtilsTest.java
@@ -0,0 +1,94 @@
+/**
+ * 
+ */
+package com.github.podd.utils.test;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.openrdf.model.URI;
+import org.openrdf.model.impl.ValueFactoryImpl;
+
+import com.github.podd.api.PoddRdfProcessor;
+import com.github.podd.api.PoddRdfProcessorFactory;
+import com.github.podd.utils.PoddRdfUtils;
+
+/**
+ * @author kutila
+ * 
+ */
+public class PoddRdfUtilsTest
+{
+    private String[] testBGPArray = { "?s ?p ?o", "[BGP-String]" };
+    private String[] testWhereArray = { "?s ?p ?o", "[WHERE-String]" };
+    private String[] testGroupByArray = { "", "[GROUPBY-String]" };
+    
+    /**
+     * Tests Building a SPARQL construct query using a mock PODD RDF Processor Factory
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testBuildSparqlConstructQuery() throws Exception
+    {
+        
+        for(int i = 0; i < 2; i++)
+        {
+            final PoddRdfProcessorFactory<PoddRdfProcessor> mockFactory = Mockito.mock(PoddRdfProcessorFactory.class);
+            Mockito.when(mockFactory.getSPARQLConstructBGP()).thenReturn(this.testBGPArray[i]);
+            Mockito.when(mockFactory.getSPARQLConstructWhere()).thenReturn(this.testWhereArray[i]);
+            Mockito.when(mockFactory.getSPARQLGroupBy()).thenReturn(this.testGroupByArray[i]);
+            
+            final String sparql = PoddRdfUtils.buildSparqlConstructQuery(mockFactory);
+            Assert.assertNotNull(sparql);
+            Assert.assertFalse(sparql.isEmpty());
+            
+            String expectedSparql =
+                    "CONSTRUCT { " + this.testBGPArray[i] + " } WHERE { " + this.testWhereArray[i] + " }";
+            if(!this.testGroupByArray[i].isEmpty())
+            {
+                expectedSparql = expectedSparql + " GROUP BY " + this.testGroupByArray[i];
+            }
+            
+            Assert.assertEquals("SPARQL Query was not as expected", expectedSparql, sparql);
+        }
+    }
+    
+    /**
+     * Tests Building a SPARQL construct query using a mock PODD RDF Processor Factory and a subject
+     * URI
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testBuildSparqlConstructQueryUsingSubject() throws Exception
+    {
+        final URI[] subjects =
+                { ValueFactoryImpl.getInstance().createURI("http://p.org/sub1"),
+                        ValueFactoryImpl.getInstance().createURI("http://p.org/sub2") };
+        
+        for(int i = 0; i < 2; i++)
+        {
+            final PoddRdfProcessorFactory<PoddRdfProcessor> mockFactory = Mockito.mock(PoddRdfProcessorFactory.class);
+            Mockito.when(mockFactory.getSPARQLConstructBGP()).thenReturn(this.testBGPArray[i]);
+            Mockito.when(mockFactory.getSPARQLConstructWhere(subjects[i])).thenReturn(
+                    this.testWhereArray[i] + subjects[i]);
+            Mockito.when(mockFactory.getSPARQLGroupBy()).thenReturn(this.testGroupByArray[i]);
+            
+            final String sparql = PoddRdfUtils.buildSparqlConstructQuery(mockFactory, subjects[i]);
+            Assert.assertNotNull(sparql);
+            Assert.assertFalse(sparql.isEmpty());
+            
+            String expectedSparql =
+                    "CONSTRUCT { " + this.testBGPArray[i] + " } WHERE { " + this.testWhereArray[i] + subjects[i] + " }";
+            if(!this.testGroupByArray[i].isEmpty())
+            {
+                expectedSparql = expectedSparql + " GROUP BY " + this.testGroupByArray[i];
+            }
+            
+            Assert.assertEquals("SPARQL Query was not as expected", expectedSparql, sparql);
+        }
+    }
+    
+}
