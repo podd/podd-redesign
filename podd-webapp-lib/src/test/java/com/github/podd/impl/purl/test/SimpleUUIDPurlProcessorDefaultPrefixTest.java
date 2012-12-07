bb5854ef25de3f8794c939345a499b00d1a34c7e
Kutila Gunasekera <kutila@gmail.com>
2012-12-07 15:14:17 +1000

    add concrete tests for handleTranslation with parent URI

--- podd-webapp-lib/src/test/java/com/github/podd/impl/purl/test/SimpleUUIDPurlProcessorDefaultPrefixTest.java
+++ podd-webapp-lib/src/test/java/com/github/podd/impl/purl/test/SimpleUUIDPurlProcessorDefaultPrefixTest.java
@@ -3,9 +3,13 @@
  */
 package com.github.podd.impl.purl.test;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.openrdf.model.URI;
+import org.openrdf.model.impl.ValueFactoryImpl;
 
 import com.github.podd.api.purl.PoddPurlProcessor;
+import com.github.podd.api.purl.PoddPurlReference;
 import com.github.podd.api.purl.test.AbstractPoddPurlProcessorTest;
 import com.github.podd.impl.purl.SimpleUUIDPurlProcessor;
 
@@ -28,4 +32,31 @@ public class SimpleUUIDPurlProcessorDefaultPrefixTest extends AbstractPoddPurlPr
         // multiple PurlProcessors use the same prefix
         return purl.stringValue().startsWith(SimpleUUIDPurlProcessor.DEFAULT_PREFIX);
     }
+    
+    /**
+     * Tests handleTranslation(inputUri, parentUri) with a valid Parent URI.
+     * Translation consists of replacing the temporary URI prefix with the parent URI, which
+     * is a unique prefix.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testHandleTranslationWithParentUriSuccessful() throws Exception
+    {
+        this.purlProcessor.addTemporaryUriHandler(this.prefixUrnTemp);
+        final URI tempUriUrnTemp = ValueFactoryImpl.getInstance().createURI(this.prefixUrnTemp + "artifact:1482");
+        final URI parentUri =
+                ValueFactoryImpl.getInstance().createURI(SimpleUUIDPurlProcessor.DEFAULT_PREFIX + "S0ME-UN1QUE-ID/");
+        
+        final PoddPurlReference purlReference = this.purlProcessor.handleTranslation(tempUriUrnTemp, parentUri);
+        
+        Assert.assertNotNull(purlReference);
+        Assert.assertEquals(tempUriUrnTemp, purlReference.getTemporaryURI());
+        Assert.assertTrue(this.isPurlGeneratedFromTemp(purlReference.getPurlURI(), tempUriUrnTemp));
+        
+        Assert.assertEquals("Not the expected Purl", 
+                SimpleUUIDPurlProcessor.DEFAULT_PREFIX + "S0ME-UN1QUE-ID/artifact:1482", 
+                purlReference.getPurlURI().stringValue());
+    }
+    
 }
--- podd-webapp-lib/src/test/java/com/github/podd/impl/purl/test/SimpleUUIDPurlProcessorNonDefaultPrefixTest.java
+++ podd-webapp-lib/src/test/java/com/github/podd/impl/purl/test/SimpleUUIDPurlProcessorNonDefaultPrefixTest.java
@@ -3,9 +3,13 @@
  */
 package com.github.podd.impl.purl.test;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.openrdf.model.URI;
+import org.openrdf.model.impl.ValueFactoryImpl;
 
 import com.github.podd.api.purl.PoddPurlProcessor;
+import com.github.podd.api.purl.PoddPurlReference;
 import com.github.podd.api.purl.test.AbstractPoddPurlProcessorTest;
 import com.github.podd.impl.purl.SimpleUUIDPurlProcessor;
 
@@ -16,7 +20,7 @@ import com.github.podd.impl.purl.SimpleUUIDPurlProcessor;
 public class SimpleUUIDPurlProcessorNonDefaultPrefixTest extends AbstractPoddPurlProcessorTest
 {
     
-    private final String purlPrefix = "urn:example:purl";
+    private final String purlPrefix = "urn:example:purl:";
     
     @Override
     protected PoddPurlProcessor getNewPoddPurlProcessor()
@@ -31,5 +35,31 @@ public class SimpleUUIDPurlProcessorNonDefaultPrefixTest extends AbstractPoddPur
         // multiple PurlProcessors use the same prefix
         return purl.stringValue().startsWith(this.purlPrefix);
     }
-    
+ 
+    /**
+     * Tests handleTranslation(inputUri, parentUri) with a valid Parent URI.
+     * Translation consists of replacing the temporary URI prefix with the parent URI, which
+     * is a unique prefix.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testHandleTranslationWithParentUriSuccessful() throws Exception
+    {
+        this.purlProcessor.addTemporaryUriHandler(this.prefixUrnTemp);
+        final URI tempUriUrnTemp = ValueFactoryImpl.getInstance().createURI(this.prefixUrnTemp + "artifact:1482");
+        final URI parentUri =
+                ValueFactoryImpl.getInstance().createURI(this.purlPrefix + "S0ME-UN1QUE-ID/");
+        
+        final PoddPurlReference purlReference = this.purlProcessor.handleTranslation(tempUriUrnTemp, parentUri);
+        
+        Assert.assertNotNull(purlReference);
+        Assert.assertEquals(tempUriUrnTemp, purlReference.getTemporaryURI());
+        Assert.assertTrue(this.isPurlGeneratedFromTemp(purlReference.getPurlURI(), tempUriUrnTemp));
+        
+        Assert.assertEquals("Not the expected Purl", 
+                this.purlPrefix + "S0ME-UN1QUE-ID/artifact:1482", 
+                purlReference.getPurlURI().stringValue());
+    }
+     
 }
