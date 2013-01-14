0145862e9a0e502c74e68f01d4e58fe39e8b7e18
Peter Ansell <p_ansell@yahoo.com>
2013-01-14 11:00:44 +1000

    add AbstractResourceImplTest

---
+++ podd-webapp-lib/src/test/java/com/github/podd/restlet/test/AbstractResourceImplTest.java
@@ -0,0 +1,104 @@
+package com.github.podd.restlet.test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.restlet.Component;
+import org.restlet.data.Protocol;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.ansell.propertyutil.PropertyUtil;
+import com.github.podd.restlet.ApplicationUtils;
+import com.github.podd.restlet.PoddWebServiceApplication;
+import com.github.podd.restlet.PoddWebServiceApplicationImpl;
+
+/**
+ * Abstract test implementation that contains common components required by resource implementation
+ * tests, including setting up the application and component, along with the TEST_PORT number to use
+ * in the tests.
+ * 
+ * @author Peter Ansell p_ansell@yahoo.com
+ */
+public class AbstractResourceImplTest
+{
+    /**
+     * Determines the TEST_PORT number to use for the test server
+     */
+    protected static final int TEST_PORT = 8182;
+    
+    protected final Logger log = LoggerFactory.getLogger(this.getClass());
+    
+    /**
+     * A constant used to make requests that require admin privileges easier to recognise inside
+     * tests.
+     */
+    protected final boolean testWithAdminPrivileges = true;
+    
+    /**
+     * A constant used to make requests that do not require admin privileges easier to recognise
+     * inside tests.
+     */
+    protected final boolean testNoAdminPrivileges = false;
+    
+    private Component component;
+    
+    public AbstractResourceImplTest()
+    {
+        super();
+    }
+    
+    /**
+     * Create a new server for each test.
+     * 
+     * State will only be shared when they use a common database.
+     * 
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception
+    {
+        this.component = new Component();
+        
+        // Add a new HTTP server listening on the given TEST_PORT.
+        this.component.getServers().add(Protocol.HTTP, AbstractResourceImplTest.TEST_PORT);
+        
+        this.component.getClients().add(Protocol.CLAP);
+        this.component.getClients().add(Protocol.HTTP);
+        
+        final PoddWebServiceApplication nextApplication = new PoddWebServiceApplicationImpl();
+        
+        // Attach the sample application.
+        this.component.getDefaultHost().attach(
+        // PropertyUtil.get(OasProps.PROP_WS_URI_PATH, OasProps.DEF_WS_URI_PATH),
+                "/podd",
+                nextApplication);
+        
+        // The application cannot be setup properly until it is attached, as it requires
+        // Application.getContext() to not return null
+        ApplicationUtils.setupApplication(nextApplication, nextApplication.getContext());
+        
+        // Start the component.
+        this.component.start();
+    }
+    
+    /**
+     * Stop and nullify the test server object after each test.
+     * 
+     * NOTE: Does not clear any databases.
+     * 
+     * @throws Exception
+     */
+    @After
+    public void tearDown() throws Exception
+    {
+        // Stop the component
+        if(this.component != null)
+        {
+            this.component.stop();
+        }
+        
+        // nullify the reference to the component
+        this.component = null;
+    }
+    
+}
---
+++ podd-webapp-lib/src/test/java/com/github/podd/restlet/test/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package containing tests of the Restlet functionality.
+ * 
+ * @author Peter Ansell p_ansell@yahoo.com
+ * 
+ */
+package com.github.podd.restlet.test;
