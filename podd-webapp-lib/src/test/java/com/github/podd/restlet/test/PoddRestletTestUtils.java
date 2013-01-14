161b9326642307187357e827a45d631d249e459d
Peter Ansell <p_ansell@yahoo.com>
2013-01-14 11:56:01 +1000

    add PoddRestletTestUtils from restlet-utils:0.1-SNAPSHOT:test-jar

---
+++ podd-webapp-lib/src/test/java/com/github/podd/restlet/test/PoddRestletTestUtils.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.github.podd.restlet.test;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.ansell.restletutils.test.RestletTestUtils;
+
+/**
+ * Test utility methods specific to Podd should be located in this class.
+ * 
+ * @author Peter Ansell p_ansell@yahoo.com
+ */
+public class PoddRestletTestUtils extends RestletTestUtils
+{
+    private static final Logger log = LoggerFactory.getLogger(PoddRestletTestUtils.class);
+    
+
+}
