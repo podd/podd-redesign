2565a1df87e05782fd404afb9258ea1c5707d221
Peter Ansell <p_ansell@yahoo.com>
2013-02-08 10:42:37 +1000

    add podd-client-restlet module as first implementation of Podd Client API

---
+++ podd-client-restlet/src/main/java/com/github/podd/client/impl/restlet/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Implementation of the PODD Client API using Restlet.
+ * 
+ * @author Peter Ansell p_ansell@yahoo.com
+ * 
+ */
+package com.github.podd.client.impl.restlet;
---
+++ podd-client-restlet/src/test/java/com/github/podd/client/impl/restlet/test/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Tests for the Restlet implementation of the PODD Client API.
+ * 
+ * @author Peter Ansell p_ansell@yahoo.com
+ */
+package com.github.podd.client.impl.restlet.test;
