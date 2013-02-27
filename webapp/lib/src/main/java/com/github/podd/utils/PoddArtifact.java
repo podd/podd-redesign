cc1566ee7ce5ea8c8688c2d84f50be595aef171e
Peter Ansell <p_ansell@yahoo.com>
2013-02-27 11:44:19 +1000

    undeprecate PoddArtifact and add a clear usage scenario for where it can be used

--- webapp/lib/src/main/java/com/github/podd/utils/PoddArtifact.java
+++ webapp/lib/src/main/java/com/github/podd/utils/PoddArtifact.java
@@ -3,21 +3,20 @@
  */
 package com.github.podd.utils;
 
-import org.openrdf.model.Model;
 import org.openrdf.model.URI;
+import org.semanticweb.owlapi.model.IRI;
 
 /**
  * Simple class to contain details about a Podd Artifact (i.e. Projects at present).
  * 
+ * This must only be used at the last stage before presenting results to users. For all other cases,
+ * {@link InferredOWLOntologyID} should be used to represent references to specific versions of
+ * artifacts, and {@link IRI} should be used to represent references to either versions or ontology
+ * IRIs before they are resolved to {@link InferredOWLOntologyID} instances.
+ * 
  * @author kutila
  * 
- *         TODO: Remove the need for this class.
- * 
- * @deprecated Only use PoddObject, and only use it when it is simpler than using
- *             {@link Model#filter(org.openrdf.model.Resource, URI, org.openrdf.model.Value, org.openrdf.model.Resource...)}
- *             .
  */
-@Deprecated
 public class PoddArtifact extends PoddObject
 {
     
