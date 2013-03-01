1f7a25a2261c8a40130c581a1e02aa4268d1a66f
Peter Ansell <p_ansell@yahoo.com>
2013-03-01 15:59:37 +1000

    add test for OntologyUtils to verify its behaviour in different circumstances

---
+++ api/src/test/java/com/github/podd/utils/test/OntologyUtilsTest.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package com.github.podd.utils.test;
+
+import java.util.Collections;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openrdf.model.Model;
+
+import com.github.podd.utils.InferredOWLOntologyID;
+import com.github.podd.utils.OntologyUtils;
+
+/**
+ * Test for OntologyUtils class that translates between RDF and InferredOWLOntologyID instances.
+ * 
+ * @author Peter Ansell p_ansell@yahoo.com
+ */
+public class OntologyUtilsTest
+{
+    
+    /**
+     * Test method for
+     * {@link com.github.podd.utils.OntologyUtils#modelToOntologyIDs(org.openrdf.model.Model)}.
+     */
+    @Ignore
+    @Test
+    public final void testModelToOntologyIDs()
+    {
+        Assert.fail("Not yet implemented"); // TODO
+    }
+    
+    /**
+     * Test method for
+     * {@link com.github.podd.utils.OntologyUtils#ontologyIDsToHandler(java.util.Collection, org.openrdf.rio.RDFHandler)}
+     * .
+     */
+    @Ignore
+    @Test
+    public final void testOntologyIDsToHandler()
+    {
+        Assert.fail("Not yet implemented"); // TODO
+    }
+    
+    /**
+     * Test method for
+     * {@link com.github.podd.utils.OntologyUtils#ontologyIDsToModel(java.util.Collection, org.openrdf.model.Model)}
+     * .
+     */
+    @Test
+    public final void testOntologyIDsToModelEmptyNull()
+    {
+        final Model ontologyIDsToModel =
+                OntologyUtils.ontologyIDsToModel(Collections.<InferredOWLOntologyID> emptyList(), (Model)null);
+        
+        Assert.assertNotNull(ontologyIDsToModel);
+        Assert.assertTrue(ontologyIDsToModel.isEmpty());
+    }
+    
+}
