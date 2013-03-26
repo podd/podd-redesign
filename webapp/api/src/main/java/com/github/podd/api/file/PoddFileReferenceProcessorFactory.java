ccc65e192fd7bb13262b7436fec4f45ddf51b2b1
Kutila Gunasekera <kutila@gmail.com>
2013-03-26 16:19:30 +1000

    add generics

--- webapp/api/src/main/java/com/github/podd/api/file/PoddFileReferenceProcessorFactory.java
+++ webapp/api/src/main/java/com/github/podd/api/file/PoddFileReferenceProcessorFactory.java
@@ -9,7 +9,7 @@ import com.github.podd.api.PoddRdfProcessorFactory;
  * @author Peter Ansell p_ansell@yahoo.com
  * 
  */
-public interface PoddFileReferenceProcessorFactory extends PoddRdfProcessorFactory<PoddFileReferenceProcessor>
+public interface PoddFileReferenceProcessorFactory extends PoddRdfProcessorFactory<PoddFileReferenceProcessor<PoddFileReference>>
 {
     
 }
