db2aaf17a1cc2226e0c3a353a3a2537026266ee3
Peter Ansell <p_ansell@yahoo.com>
2013-04-24 14:29:52 +1000

    convert another location to use temporary file instead of direct access into jar file

--- webapp/lib/src/test/java/com/github/podd/restlet/test/UploadArtifactResourceImplTest.java
+++ webapp/lib/src/test/java/com/github/podd/restlet/test/UploadArtifactResourceImplTest.java
@@ -153,18 +153,13 @@ public class UploadArtifactResourceImplTest extends AbstractResourceImplTest
         final ClientResource uploadArtifactClientResource =
                 new ClientResource(this.getUrl(PoddWebConstants.PATH_ARTIFACT_UPLOAD));
         
-        final URL fileUrl = this.getClass().getResource("/test/artifacts/basicProject-1-internal-object.rdf");
-        
-        this.log.info("The URL is {}", fileUrl);
-        Assert.assertNotNull("Null artifact file", fileUrl);
-        
-        final File artifactDiskFile = new File(fileUrl.toURI());
-        final FileRepresentation fileRep = new FileRepresentation(artifactDiskFile, MediaType.APPLICATION_RDF_XML);
-        Assert.assertNotNull("Null FileRepresentation", fileRep);
+        final Representation input =
+                this.buildRepresentationFromResource("/test/artifacts/basicProject-1-internal-object.rdf",
+                        MediaType.APPLICATION_RDF_XML);
         
         final FormDataSet form = new FormDataSet();
         form.setMultipart(true);
-        form.getEntries().add(new FormData("file", fileRep));
+        form.getEntries().add(new FormData("file", input));
         
         final Representation results =
                 RestletTestUtils.doTestAuthenticatedRequest(uploadArtifactClientResource, Method.POST, form,
