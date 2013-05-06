04d6f88f30b4aa967acd1d6fdad2363e31bd66b1
Kutila Gunasekera <kutila@gmail.com>
2013-05-06 17:10:39 +1000

    add a simple HTML page to test building "edit object" page

---
+++ webapp/lib/src/main/resources/scripts/podd_edit.js
@@ -0,0 +1,14 @@
+/**
+ * PODD : Uses jquery-RDF to analyse the current document with respect to the
+ * RDFa information it contains and populate the rdfadebug element with the
+ * resulting information
+ */
+
+
+
+// --------------------------------
+
+function loadObjectDetails() {
+	console.debug('loadObjectDetails() invoked from podd_edit.js!!!');
+}
+
