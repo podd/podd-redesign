209f3e21a9398f8378dd111b33cde615266f2de9
Kutila Gunasekera <kutila@gmail.com>
2013-05-08 16:57:23 +1000

    test edit page continuation  - add displaying of labels for field values when available  - populate drop-down lists

--- webapp/lib/src/main/resources/scripts/podd_edit.js
+++ webapp/lib/src/main/resources/scripts/podd_edit.js
@@ -72,6 +72,7 @@ function loadEditDataCallback(resultData, status, xhr) {
 	.optional('?propertyUri <http://www.w3.org/2000/01/rdf-schema#label> ?pLabel')
 	.optional('?propertyUri poddBase:hasDisplayType ?displayType')
 	.optional('?propertyUri poddBase:hasCardinality ?cardinality')
+	.optional('?pValue <http://www.w3.org/2000/01/rdf-schema#label> ?pValueLabel')
 	;
 	var bindings = myQuery.select();
 
@@ -83,7 +84,6 @@ function loadEditDataCallback(resultData, status, xhr) {
 		nextChild.propertyLabel;
 		nextChild.displayType;
 		nextChild.cardinality;
-		nextChild.value = value.pValue.value;
 		
 		if (typeof value.pLabel != 'undefined') {
 			nextChild.propertyLabel = value.pLabel.value;
@@ -97,6 +97,14 @@ function loadEditDataCallback(resultData, status, xhr) {
 		
 		if (typeof value.cardinality != 'undefined') {
 			nextChild.cardinality = value.cardinality.value;
+		}
+		
+		if (typeof value.pValueLabel != 'undefined') {
+			nextChild.displayValue = value.pValueLabel.value;
+			nextChild.valueUri = value.pValue.value;
+		} else {
+			nextChild.displayValue = value.pValue.value;
+			nextChild.valueUri = value.pValue.value;
 		}
 		
 		propertyList.push(nextChild);
@@ -115,55 +123,171 @@ function loadEditDataCallback(resultData, status, xhr) {
 	$.each(propertyList, displayEditField);
 }
 
+// these can be loaded via freemarker
+var CARD_ExactlyOne = 'http://purl.org/podd/ns/poddBase#Cardinality_Exactly_One';
+var CARD_OneOrMany = 'http://purl.org/podd/ns/poddBase#Cardinality_One_Or_Many';
+var CARD_ZeroOrMany = 'http://purl.org/podd/ns/poddBase#Cardinality_Zero_Or_Many'
+
+var DISPLAY_LongText = 'http://purl.org/podd/ns/poddBase#DisplayType_LongText';
+var DISPLAY_ShortText = 'http://purl.org/podd/ns/poddBase#DisplayType_ShortText';
+var DISPLAY_CheckBox = 'http://purl.org/podd/ns/poddBase#DisplayType_CheckBox';
+var DISPLAY_DropDown = 'http://purl.org/podd/ns/poddBase#DisplayType_DropDownList'
+var DISPLAY_AutoComplete = 'http://purl.org/podd/ns/poddBase#DisplayType_AutoComplete';
+var DISPLAY_Table = 'http://purl.org/podd/ns/poddBase#DisplayType_Table';
+	
 /*
  * Display the given field on page
  */
 function displayEditField(index, nextField) {
 	// console.debug('[' + nextField.weight + '] <' + nextField.propertyUri + '> "' + nextField.propertyLabel + '" <' +
 	//		nextField.displayType + '> <' + nextField.cardinality + '>');
+
+    var span = $('<span>');
+    span.attr('class', 'bold');
+    span.html(nextField.propertyLabel);
+
+    var li = $("<li>")
+	li.append(span);
 	
-	var html = '<li> <span class="bold">:' + nextField.propertyLabel + '</span>';
-	
-	if (nextField.cardinality == 'http://purl.org/podd/ns/poddBase#Cardinality_Exactly_One' ||
-			nextField.cardinality == 'http://purl.org/podd/ns/poddBase#Cardinality_One_Or_Many') {
-		
-		html = html + '<span icon="required"></span>';
+	// required icon
+	if (nextField.cardinality == CARD_ExactlyOne || nextField.cardinality == CARD_OneOrMany) {
+		spanRequired = $('<span>');
+		spanRequired.attr('icon', 'required');
+		li.append(spanRequired);
 	}
 	
-	if (nextField.cardinality == 'http://purl.org/podd/ns/poddBase#Cardinality_Zero_Or_Many' ||
-			nextField.cardinality == 'http://purl.org/podd/ns/poddBase#Cardinality_One_Or_Many') {
-		
-		html = html + '<a href="javascript:addField(TODO)" icon="addField" title="add ' + nextField.propertyLabel 
-		+ '"></a>';
+	// display (+) icon with action to add extra values
+	if (nextField.cardinality == CARD_ZeroOrMany || nextField.cardinality == CARD_OneOrMany) {
+		link = $('<a>');
+		link.attr('href', 'javascript:addNewEmptyField()');
+		link.attr('icon', 'addField');
+		link.attr('title', 'add ' + nextField.propertyLabel);
+		li.append(link);
 	}
 	
+	var li2 = $("<li>")
 	
-	if (nextField.displayType == 'http://purl.org/podd/ns/poddBase#DisplayType_LongText') {
-		html = html + '<input property="' + nextField.propertyUri + '"  id="prop_' + nextField.propertyLabel +
-		'" name="' + '" value="' + nextField.value + '" type="textarea">';
+	if (nextField.displayType == DISPLAY_LongText) {
+		var input = $('<input>', {
+			id: 'prop_' + nextField.propertyLabel,
+			name: 'prop_' + nextField.propertyLabel,
+		    type:'textarea',
+		    value: nextField.displayValue
+		});
+		li2.append(input);
 		
-	} else if (nextField.displayType == 'http://purl.org/podd/ns/poddBase#DisplayType_DropDownList') {
-		html = html + '<p>Drop Down List</p>';
+	} else if (nextField.displayType == DISPLAY_DropDown) {
+		var select = $('<select>', {
+			id: 'prop_' + nextField.propertyLabel,
+			name: 'prop_' + nextField.propertyLabel,
+		});
 		
-	} else if (nextField.displayType == 'http://purl.org/podd/ns/poddBase#DisplayType_CheckBox') {
-		html = html + '<input type="checkbox" name="' + nextField.propertyLabel + '" value="' + nextField.value + '">' + nextField.value;
+		var myQuery = $.rdf({
+			databank : nextDatabank
+		})
+		.prefix('poddBase', 'http://purl.org/podd/ns/poddBase#')
+		.where('<' + nextField.propertyUri + '> poddBase:hasAllowedValue ?pValue')
+		.optional('?pValue <http://www.w3.org/2000/01/rdf-schema#label> ?pDisplayValue')
+		;
+		var bindings = myQuery.select();
+//		console.debug('Found ' + bindings.length + ' bindings for query');
+		$.each(bindings, function(index, value) {
+			
+			var optionValue = value.pValue.value;
+			
+			var optionDisplayValue = value.pValue.value;
+			if (value.pDisplayValue != 'undefined') {
+				optionDisplayValue = value.pDisplayValue.value;
+			}
+			
+			var selectedVal = false;
+			console.debug(nextField.valueUri + ' against ' + optionValue);
+			if (nextField.valueUri == optionValue)	{
+				console.debug(optionValue + ' is the current selected value');
+				selectedVal = true;
+			}
+			
+			var option = $('<option>', {
+				value: optionValue,
+				text: optionDisplayValue,
+				selected: selectedVal
+			});
+			
+			select.append(option);
+		});
 		
-	} else if (nextField.displayType == 'http://purl.org/podd/ns/poddBase#DisplayType_Table') {
-		html = html + '<p>Table</p>';
+		li2.append(select);
 		
-	} else if (nextField.displayType == 'http://purl.org/podd/ns/poddBase#DisplayType_AutoComplete') {
-		html = html + '<p>auto complete</p>';
+	} else if (nextField.displayType == DISPLAY_CheckBox) {
+		checkBox = $('<p>');
+		checkBox.text('CheckBox here');
+		
+		li2.append(checkBox);
+		
+	} else if (nextField.displayType == DISPLAY_Table) {
+		checkBox = $('<p>');
+		checkBox.text('Table here please');
+
+		li2.append(checkBox);
+		
+	} else if (nextField.displayType == DISPLAY_AutoComplete) {
+		checkBox = $('<p>');
+		checkBox.text('Auto complete please');
+
+		li2.append(checkBox);
 		
 	} else { // default, short_text
-	
-		html = html + '<input property="' + nextField.propertyUri + '"  id="prop_' + nextField.propertyLabel +
-			'" name="' + '" value="' + nextField.value + '" type="text">';
+		var input = $("<input>", {
+			id: 'prop_' + nextField.propertyLabel,
+			name: 'prop_' + nextField.propertyLabel,
+			type: 'text',
+			value: nextField.displayValue
+		});
+		li2.append(input);
 	}
-	html = html + '</li>';
-	
-	$("#details ol").append(html);
-	
-	
-	
-	
+
+	li.append(li2);
+	$("#details ol").append(li);
 }
+
+
+/*
+ * Search Ontology Resource Service using AJAX, convert the RDF response to
+ * a JSON array and invoke the callback function.
+ */
+function searchOntologyService(
+		/* object with 'search term' */ request, 
+		/* function */ callbackFunction
+		) {
+
+	requestUrl = podd.baseUrl + '/search';
+
+	console.debug('Searching artifact: "' + artifactUri.toString() + '" in searchTypes: "' + request.searchTypes 
+			+ '" for terms matching "'	+ request.term + '".');
+
+	queryParams = {
+		searchterm : request.term,
+		artifacturi : artifactUri.toString(),
+		searchtypes : request.searchTypes
+	};
+
+	$.get(requestUrl, queryParams, function(data) {
+		console.debug('Response: ' + data.toString());
+		var formattedData = parsesearchresults(requestUrl, data);
+		console.debug('No. of search results = ' + formattedData.length);
+		//callbackFunction(formattedData);
+	}, 'json');
+}
+
+
+
+//simply copied from PODD-1, does not work
+function addNewEmptyField(id, type) {
+    var element = document.createElement(type);
+    element.setAttribute('id', id);
+    element.setAttribute('name', id);
+    var li = document.createElement("li");
+    li.appendChild(element);
+    document.getElementById('add_' + id).appendChild(li);
+}
+
