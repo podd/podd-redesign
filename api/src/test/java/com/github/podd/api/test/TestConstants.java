4346d9566340f3614b62f7e944dad2844f6577f2
Kutila Gunasekera <kutila@gmail.com>
2013-07-16 12:24:51 +1000

    update test numbers

--- api/src/test/java/com/github/podd/api/test/TestConstants.java
+++ api/src/test/java/com/github/podd/api/test/TestConstants.java
@@ -17,7 +17,7 @@ public interface TestConstants
     public static final int EXPECTED_TRIPLE_COUNT_PODD_USER_CONCRETE = 217;
     public static final int EXPECTED_TRIPLE_COUNT_PODD_USER_INFERRED = 34;
     
-    public static final int EXPECTED_TRIPLE_COUNT_PODD_BASE_CONCRETE = 358;
+    public static final int EXPECTED_TRIPLE_COUNT_PODD_BASE_CONCRETE = 361;
     public static final int EXPECTED_TRIPLE_COUNT_PODD_BASE_INFERRED = 71;
     
     public static final int EXPECTED_TRIPLE_COUNT_PODD_SCIENCE_CONCRETE = 1309;
