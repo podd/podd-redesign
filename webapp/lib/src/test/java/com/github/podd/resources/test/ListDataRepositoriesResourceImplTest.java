e634cc11c878e672d68d5b871fabe7beb1161a59
Peter Ansell <p_ansell@yahoo.com>
2013-07-22 10:20:09 +1000

    add content tests for rdf and html list data repositories pages

--- webapp/lib/src/test/java/com/github/podd/resources/test/ListDataRepositoriesResourceImplTest.java
+++ webapp/lib/src/test/java/com/github/podd/resources/test/ListDataRepositoriesResourceImplTest.java
@@ -3,8 +3,14 @@
  */
 package com.github.podd.resources.test;
 
+import java.io.ByteArrayInputStream;
+import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.openrdf.model.Model;
+import org.openrdf.rio.RDFFormat;
 import org.restlet.data.MediaType;
 import org.restlet.data.Method;
 import org.restlet.data.Status;
@@ -14,6 +20,8 @@ import org.restlet.resource.ResourceException;
 
 import com.github.ansell.restletutils.RestletUtilMediaType;
 import com.github.ansell.restletutils.test.RestletTestUtils;
+import com.github.podd.utils.DebugUtils;
+import com.github.podd.utils.PoddRdfConstants;
 import com.github.podd.utils.PoddWebConstants;
 
 /**
@@ -34,6 +42,16 @@ public class ListDataRepositoriesResourceImplTest extends AbstractResourceImplTe
         Representation result =
                 RestletTestUtils.doTestAuthenticatedRequest(dataRepositoriesClientResource, Method.GET, null,
                         MediaType.TEXT_HTML, Status.SUCCESS_OK, this.testWithAdminPrivileges);
+        final String body = result.getText();
+        
+        // System.out.println(body);
+        
+        // verify:
+        assertFreemarker(body);
+        
+        Assert.assertFalse(body.contains("No data repositories currently available"));
+        
+        Assert.assertTrue(body.contains("alias_local_ssh"));
     }
     
     /**
@@ -89,6 +107,13 @@ public class ListDataRepositoriesResourceImplTest extends AbstractResourceImplTe
         Representation result =
                 RestletTestUtils.doTestAuthenticatedRequest(dataRepositoriesClientResource, Method.GET, null,
                         MediaType.APPLICATION_RDF_XML, Status.SUCCESS_OK, this.testWithAdminPrivileges);
+        
+        final String body = result.getText();
+        
+        // verify:
+        final Model model = this.assertRdf(new StringReader(body), RDFFormat.RDFXML, 3);
+        
+        // DebugUtils.printContents(model);
     }
     
     /**
@@ -144,6 +169,13 @@ public class ListDataRepositoriesResourceImplTest extends AbstractResourceImplTe
         Representation result =
                 RestletTestUtils.doTestAuthenticatedRequest(dataRepositoriesClientResource, Method.GET, null,
                         RestletUtilMediaType.APPLICATION_RDF_JSON, Status.SUCCESS_OK, this.testWithAdminPrivileges);
+        
+        final String body = result.getText();
+        
+        // verify:
+        final Model model = this.assertRdf(new StringReader(body), RDFFormat.RDFJSON, 3);
+        
+        // DebugUtils.printContents(model);
     }
     
     /**
