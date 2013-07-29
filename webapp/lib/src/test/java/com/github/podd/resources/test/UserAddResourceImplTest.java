d18419311b89d3becbfea97fc30fa3ad22103a20
Kutila Gunasekera <kutila@gmail.com>
2013-07-29 12:26:25 +1000

    test PODD User Identifier has to be same as email.

--- webapp/lib/src/test/java/com/github/podd/resources/test/UserAddResourceImplTest.java
+++ webapp/lib/src/test/java/com/github/podd/resources/test/UserAddResourceImplTest.java
@@ -16,6 +16,7 @@ import org.openrdf.model.Model;
 import org.openrdf.model.URI;
 import org.openrdf.model.impl.LinkedHashModel;
 import org.openrdf.model.vocabulary.RDF;
+import org.openrdf.model.vocabulary.RDFS;
 import org.openrdf.rio.RDFFormat;
 import org.openrdf.rio.Rio;
 import org.restlet.data.MediaType;
@@ -24,6 +25,7 @@ import org.restlet.data.Status;
 import org.restlet.representation.Representation;
 import org.restlet.representation.StringRepresentation;
 import org.restlet.resource.ClientResource;
+import org.restlet.resource.ResourceException;
 
 import com.github.ansell.restletutils.SesameRealmConstants;
 import com.github.ansell.restletutils.test.RestletTestUtils;
@@ -177,4 +179,60 @@ public class UserAddResourceImplTest extends AbstractResourceImplTest
                 resultsModel.filter(null, SesameRealmConstants.OAS_USERIDENTIFIER, null).subjects().iterator().next().stringValue());
     }
 
+    @Test
+    public void testErrorAddUserWithIdentifierNotMatchingEmailRdf() throws Exception
+    {
+        final MediaType mediaType = MediaType.APPLICATION_RDF_XML;
+        final RDFFormat format = Rio.getWriterFormatForMIMEType(mediaType.getName(), RDFFormat.RDFXML);
+        
+        // prepare: create a Model of user
+        final String testIdentifier = "wrong@restlet-test.org";
+        final String testEmail = "testuser@restlet-test.org";
+        final String testPassword = "testpassword";
+        final String testFirstName = "First";
+        final String testLastName = "Last";
+        
+        final Model userInfoModel = new LinkedHashModel();
+        final URI tempUserUri = PoddRdfConstants.VF.createURI("urn:temp:user");
+        userInfoModel.add(tempUserUri, SesameRealmConstants.OAS_USERIDENTIFIER,
+                PoddRdfConstants.VF.createLiteral(testEmail));
+        userInfoModel.add(tempUserUri, SesameRealmConstants.OAS_USERSECRET,
+                PoddRdfConstants.VF.createLiteral(testPassword));
+        userInfoModel.add(tempUserUri, SesameRealmConstants.OAS_USERFIRSTNAME,
+                PoddRdfConstants.VF.createLiteral(testFirstName));
+        userInfoModel.add(tempUserUri, SesameRealmConstants.OAS_USERLASTNAME,
+                PoddRdfConstants.VF.createLiteral(testLastName));
+        userInfoModel
+                .add(tempUserUri, SesameRealmConstants.OAS_USEREMAIL, PoddRdfConstants.VF.createLiteral(testIdentifier));
+        
+        // prepare: add 'Authenticated User' Role
+        final URI authenticatedRoleMapping =
+                PoddRdfConstants.VF.createURI("urn:podd:rolemapping:", UUID.randomUUID().toString());
+        userInfoModel.add(authenticatedRoleMapping, RDF.TYPE, SesameRealmConstants.OAS_ROLEMAPPING);
+        userInfoModel.add(authenticatedRoleMapping, SesameRealmConstants.OAS_ROLEMAPPEDUSER, tempUserUri);
+        userInfoModel.add(authenticatedRoleMapping, SesameRealmConstants.OAS_ROLEMAPPEDROLE,
+                PoddRoles.AUTHENTICATED.getURI());
+        
+        final ClientResource userAddClientResource = new ClientResource(this.getUrl(PoddWebConstants.PATH_USER_ADD));
+        
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        Rio.write(userInfoModel, out, format);
+        final Representation input = new StringRepresentation(out.toString(), mediaType);
+        
+        try
+        {
+            RestletTestUtils.doTestAuthenticatedRequest(userAddClientResource, Method.POST, input, mediaType,
+                    Status.SUCCESS_OK, this.testWithAdminPrivileges);
+            Assert.fail("Should have failed due to mismatching identifier");
+        }
+        catch(final ResourceException e)
+        {
+            // verify: the cause (simple string matching, not checking for valid RDF content)
+            Assert.assertEquals(Status.CLIENT_ERROR_BAD_REQUEST, e.getStatus());
+            final String body = userAddClientResource.getResponseEntity().getText();
+            Assert.assertTrue("Expected cause is missing", body.contains("User Email has to be the same as User Identifier"));
+        }
+    }
+    
+    
 }
