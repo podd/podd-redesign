dfd94d79925a563de35f7ab3a5c130b98b3ab12d
Peter Ansell <p_ansell@yahoo.com>
2013-08-21 09:20:40 +1000

    quick cleanup of empty comments

--- webapp/api/src/test/java/com/github/podd/utils/test/PoddRdfUtilsTest.java
+++ webapp/api/src/test/java/com/github/podd/utils/test/PoddRdfUtilsTest.java
@@ -14,9 +14,6 @@
  * You should have received a copy of the GNU Affero General Public License along with this program.
  * If not, see <http://www.gnu.org/licenses/>.
  */
- /**
- * 
- */
 package com.github.podd.utils.test;
 
 import org.junit.Assert;
--- webapp/lib/src/main/java/com/github/podd/restlet/PoddSesameRealmImpl.java
+++ webapp/lib/src/main/java/com/github/podd/restlet/PoddSesameRealmImpl.java
@@ -14,9 +14,6 @@
  * You should have received a copy of the GNU Affero General Public License along with this program.
  * If not, see <http://www.gnu.org/licenses/>.
  */
- /**
- * 
- */
 package com.github.podd.restlet;
 
 import java.util.AbstractMap;
