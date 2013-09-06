5a3783ea3bb57363cee35e44c166f011111ed97b
Kutila Gunasekera <kutila@gmail.com>
2013-09-06 16:55:13 +1000

    add restlet resource test for delete artifact

---
+++ webapp/lib/src/test/java/com/github/podd/resources/test/DeleteArtifactResourceImplTest.java
@@ -0,0 +1,75 @@
+/**
+ * PODD is an OWL ontology database used for scientific project management
+ * 
+ * Copyright (C) 2009-2013 The University Of Queensland
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.podd.resources.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.restlet.data.MediaType;
+import org.restlet.data.Method;
+import org.restlet.data.Status;
+import org.restlet.resource.ClientResource;
+import org.restlet.resource.ResourceException;
+
+import com.github.ansell.restletutils.test.RestletTestUtils;
+import com.github.podd.api.test.TestConstants;
+import com.github.podd.utils.InferredOWLOntologyID;
+import com.github.podd.utils.PoddWebConstants;
+
+/**
+ * @author kutila
+ * 
+ */
+public class DeleteArtifactResourceImplTest extends AbstractResourceImplTest
+{
+    @Test
+    public void testDeleteArtifactBasicRdf() throws Exception
+    {
+        // prepare: add an artifact
+        final InferredOWLOntologyID artifactID =
+                this.loadTestArtifact(TestConstants.TEST_ARTIFACT_20130206, MediaType.APPLICATION_RDF_TURTLE);
+        
+        
+        final ClientResource deleteArtifactClientResource =
+                new ClientResource(this.getUrl(PoddWebConstants.PATH_ARTIFACT_DELETE));
+        
+        deleteArtifactClientResource.addQueryParameter(PoddWebConstants.KEY_ARTIFACT_IDENTIFIER, artifactID
+                .getOntologyIRI().toString());
+        deleteArtifactClientResource.addQueryParameter(PoddWebConstants.KEY_ARTIFACT_VERSION_IDENTIFIER, artifactID
+                .getVersionIRI().toString());
+        
+        
+        RestletTestUtils.doTestAuthenticatedRequest(deleteArtifactClientResource, Method.DELETE, null,
+                MediaType.APPLICATION_RDF_XML, Status.SUCCESS_NO_CONTENT, this.testWithAdminPrivileges);        
+        
+        // verify: try to retrieve deleted artifact
+        final ClientResource getArtifactClientResource =
+                new ClientResource(this.getUrl(PoddWebConstants.PATH_ARTIFACT_GET_BASE));
+        getArtifactClientResource.addQueryParameter(PoddWebConstants.KEY_ARTIFACT_IDENTIFIER, artifactID
+                .getOntologyIRI().toString());
+        try
+        {
+            RestletTestUtils.doTestAuthenticatedRequest(getArtifactClientResource, Method.GET, null,
+                    MediaType.APPLICATION_RDF_XML, Status.CLIENT_ERROR_NOT_FOUND, this.testWithAdminPrivileges);
+            Assert.fail("Should have failed with a NOT_FOUND error");
+        }
+        catch(ResourceException e)
+        {
+            Assert.assertNotNull(e);
+        }
+    }    
+    
+}
