76b76744701425aade55b07f9e6168b535a2a992
Peter Ansell <p_ansell@yahoo.com>
2013-09-18 09:41:42 +1000

    fix testCacheSchemaOntologyWithOneImport

--- api/src/test/java/com/github/podd/api/test/TestConstants.java
+++ api/src/test/java/com/github/podd/api/test/TestConstants.java
@@ -24,20 +24,20 @@ public interface TestConstants
     
     /* Expected number of triples in schema ontologies */
     
-    public static final int EXPECTED_TRIPLE_COUNT_DC_TERMS_CONCRETE = 39;
-    public static final int EXPECTED_TRIPLE_COUNT_DC_TERMS_INFERRED = 7;
+    public static final int EXPECTED_TRIPLE_COUNT_DC_TERMS_CONCRETE = 43;
+    public static final int EXPECTED_TRIPLE_COUNT_DC_TERMS_INFERRED = 8;
     
     public static final int EXPECTED_TRIPLE_COUNT_FOAF_CONCRETE = 38;
-    public static final int EXPECTED_TRIPLE_COUNT_FOAF_INFERRED = 18;
+    public static final int EXPECTED_TRIPLE_COUNT_FOAF_INFERRED = 19;
     
     public static final int EXPECTED_TRIPLE_COUNT_PODD_USER_CONCRETE = 217;
-    public static final int EXPECTED_TRIPLE_COUNT_PODD_USER_INFERRED = 34;
+    public static final int EXPECTED_TRIPLE_COUNT_PODD_USER_INFERRED = 35;
     
     public static final int EXPECTED_TRIPLE_COUNT_PODD_BASE_CONCRETE = 352;
-    public static final int EXPECTED_TRIPLE_COUNT_PODD_BASE_INFERRED = 69;
+    public static final int EXPECTED_TRIPLE_COUNT_PODD_BASE_INFERRED = 70;
     
-    public static final int EXPECTED_TRIPLE_COUNT_PODD_SCIENCE_CONCRETE = 1309;
-    public static final int EXPECTED_TRIPLE_COUNT_PODD_SCIENCE_INFERRED = 208;
+    public static final int EXPECTED_TRIPLE_COUNT_PODD_SCIENCE_CONCRETE = 1338;
+    public static final int EXPECTED_TRIPLE_COUNT_PODD_SCIENCE_INFERRED = 209;
     
     public static final int EXPECTED_TRIPLE_COUNT_PODD_PLANT_CONCRETE = 243;
     public static final int EXPECTED_TRIPLE_COUNT_PODD_PLANT_INFERRED = 346;
