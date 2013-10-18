84aa5c2f5e2feca3e5ebe2b2e9c465e072f99dc3
Peter Ansell <p_ansell@yahoo.com>
2013-10-18 12:06:52 +1100

    Implement part of populateProjectUriMap
    Basic algorithm to perform one query for each artifact to get its
    project label.
    
    Requires a new getTopObject method that returns a model containing the
    details of the top object.

--- client/hrppc/src/main/java/au/org/plantphenomics/podd/HrppcPoddClient.java
+++ client/hrppc/src/main/java/au/org/plantphenomics/podd/HrppcPoddClient.java
@@ -64,16 +64,22 @@ public class HrppcPoddClient extends RestletPoddClientImpl
 	/**
 	 * Parses the given PlantScan project/experiment/tray/pot list and inserts the 
 	 * items into PODD where they do not exist.
+	 *
+	 * TODO: Should this process create new projects where they do not already exist? 
+	 * Ideally they should be created and roles assigned before this process, but could be fine to do that in here
 	 */
 	public void uploadPlantScanList(InputStream in) throws IOException, PoddClientException
 	{
 		List<InferredOWLOntologyID> currentUnpublishedArtifacts = this.listUnpublishedArtifacts();
 		
 		// Keep a queue so that we only need to update each project once for this operation to succeed
-		ConcurrentMap<InferredOWLOntologyID, Model> uploadQueue = new ConcurrentLinkedHashMap<>();
+		ConcurrentMap<InferredOWLOntologyID, Model> uploadQueue = new ConcurrentHashMap<>();
 		
-		// TODO: Implement getTopObject(InferredOWLOntologyID) so that the top object for each can be 
-		// scanned easily to determine its name which is required, by convention, here
+		// Map starting at project name strings and ending with both the URI of the project and the artifact
+		ConcurrentMap<String, ConcurrentMap<URI, InferredOWLOntologyID>> projectUriMap = new ConcurrentHashMap<>();
+		
+		// Map known project names to their URIs, as the URIs are needed to create statements internally
+		populateProjectUriMap(currentUnpublishedArtifacts, projectUriMap);
 		
 		// TODO: Implement getObjectsByType(InferredOWLOntology, URI) so that experiments etc can be found easily 
 		// and the identifier can be mapped as necessary to the identifier in the header
@@ -100,12 +106,84 @@ public class HrppcPoddClient extends RestletPoddClientImpl
                         else 
                         {
                         	// Process the next line and add it to the upload queue 
-                        	processPlantScanLine(headers, Arrays.asList(nextLine), currentUnpublishedArtifacts, uploadQueue);
+                        	processPlantScanLine(headers, Arrays.asList(nextLine), projectNameMap, uploadQueue);
                         }
                 }
         }
         
-        public void processPlantScanLine(List<String> headers, List<String> nextLine, List<InferredOWLOntologyID> currentUnpublishedArtifacts, ConcurrentMap<InferredOWLOntologyID, Model> uploadQueue)
+        public void populateProjectUriMap(List<InferredOWLOntologyID> currentUnpublishedArtifacts, ConcurrentMap<String, ConcurrentMap<URI, InferredOWLOntologyID>> projectUriMap)
+        {
+		for(InferredOWLOntologyID nextArtifact : currentUnpublishedArtifacts) 
+		{
+			// TODO: Implement getTopObject(InferredOWLOntologyID) so that the top object for each can be 
+			// scanned easily to determine its name which is required, by convention, here
+			Model nextTopObject = getTopObject(nextArtifact);
+			
+			Model types = nextTopObject.filter(null, RDF.TYPE, PoddRdfConstants.PODD_SCIENCE_PROJECT);
+			if(types.isEmpty())
+			{
+				// We only map project based artifacts, others are ignored with log messages here
+				this.log.info("Found a non-project based artifact, ignoring as it isn't relevant here: {}", nextArtifact);
+			}
+			else if(types.subjects().size() > 1)
+			{
+				// We only map single-project based artifacts, others are ignored with log messages here
+				this.log.error("Found multiple projects for an artifact: {} {}", nextArtifact, types.subjects());
+			}
+			else
+			{
+				Resource project = types.subjects().iterator().next();
+				
+				if(project instanceof URI)
+				{
+					Model label = nextTopObject.filter(project, RDFS.LABEL, null);
+					
+					if(label.isEmpty())
+					{
+						this.log.error("Project did not have a label: {} {}", nextArtifact, project);
+					}
+					else
+					{
+						for(Value nextLabel : label.objects())
+						{
+							if(nextLabel instanceof Literal)
+							{
+								String nextLabelString = nextLabel.stringValue();
+								ConcurrentMap<URI, InferredOWLOntologyID> labelMap = new ConcurrentHashMap<>();
+								ConcurrentMap<URI, InferredOWLOntologyID> putIfAbsent = projectUriMap.putIfAbsent(nextLabelString, labelMap);
+								if(putIfAbsent != null)
+								{
+									this.log.error("Found duplicate project name, inconsistent results may follow: {} {} {}", nextArtifact, project, nextLabel);
+									// Overwrite our reference with the one that already existed
+									labelMap = putIfAbsent;
+								}
+								InferredOWLOntologyID existingArtifact = labelMap.putIfAbsent((URI)project, nextArtifact);
+								// Check for the case where project name maps to different artifacts
+								if(existingArtifact != null && !existingArtifact.equals(nextArtifact)) 
+								{
+									this.log.error("Found duplicate project name across different projects, inconsistent results may follow: {} {} {} {}", nextArtifact, existingArtifact, project, nextLabel);
+								}
+							}
+							else
+							{
+								this.log.error("Project had a non-literal label: {} {} {}", nextArtifact, project, nextLabel);
+							}
+						}
+					}
+				}
+				else
+				{
+					// We only map URI references, as blank nodes which are allowable, cannot be reserialised to update the artifact, and should not exist
+					this.log.error("Found non-URI project reference for an artifact: {} {}", nextArtifact, types.subjects());
+				}
+			}
+		}
+        }
+        
+        /**
+         * Process a single line from the input file, using the given headers as the definitions for the line. 
+         */
+        public void processPlantScanLine(List<String> headers, List<String> nextLine, ConcurrentMap<String, ConcurrentMap<URI, InferredOWLOntologyID>> projectUriMap, ConcurrentMap<InferredOWLOntologyID, Model> uploadQueue)
         {
         	
         }
