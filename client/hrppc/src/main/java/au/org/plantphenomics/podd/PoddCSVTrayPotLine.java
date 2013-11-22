bf1c1d3f51103c1b74e1fed8add48465cba26f36
Peter Ansell <p_ansell@yahoo.com>
2013-11-22 11:27:49 +1100

    add container for information on each line of the PODD-CSV file

---
+++ client/hrppc/src/main/java/au/org/plantphenomics/podd/PoddCSVTrayPotLine.java
@@ -0,0 +1,54 @@
+/**
+ * PODD is an OWL ontology database used for scientific project management
+ * 
+ * Copyright (C) 2009-2013 The University Of Queensland
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+package au.org.plantphenomics.podd;
+
+/**
+ * Container for a row in the PODD-CSV file containing each of the supported columns
+ * 
+ * @author Peter Ansell p_ansell@yahoo.com
+ * 
+ */
+public class PoddCSVTrayPotLine
+{
+    public String year;
+    public String projectNumber;
+    public String experimentNumber;
+    public String genus;
+    public String species;
+    public String potNumber;
+    public String trayNumber;
+    public String potNumberTray;
+    public String columnNumberTray;
+    public String columnLetter;
+    public String rowNumberTray;
+    public String rowNumberRep;
+    public String columnNumberRep;
+    public String columnNumber;
+    public String trayID;
+    public String trayNotes;
+    public String trayRowNumber;
+    public String trayTypeName;
+    public String position;
+    public String plantID;
+    public String plantLineNumber;
+    public String plantLineName;
+    public String plantNote;
+    public String control;
+    public String replicateNumber;
+    public String potReplicateNumber;
+    
+}
