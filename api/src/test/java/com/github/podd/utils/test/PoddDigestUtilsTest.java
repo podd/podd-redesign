49c4b5bff99e174926bb34507a1a7163160a094d
Peter Ansell <p_ansell@yahoo.com>
2014-02-27 12:11:22 +1100

    fix digest utils test

--- api/src/test/java/com/github/podd/utils/test/PoddDigestUtilsTest.java
+++ api/src/test/java/com/github/podd/utils/test/PoddDigestUtilsTest.java
@@ -16,10 +16,7 @@
  */
 package com.github.podd.utils.test;
 
-import static org.junit.Assert.*;
-
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -34,6 +31,7 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import com.github.podd.utils.PoddDigestUtils;
+import com.github.podd.utils.PoddDigestUtils.Algorithm;
 
 public class PoddDigestUtilsTest
 {
@@ -45,46 +43,47 @@ public class PoddDigestUtilsTest
     @Before
     public void setUp() throws Exception
     {
-        testDir = tempDir.newFolder("podddigestutils").toPath();
+        this.testDir = this.tempDir.newFolder("podddigestutils").toPath();
     }
     
     @After
     public void tearDown() throws Exception
     {
-        testDir = null;
+        this.testDir = null;
     }
     
     @Test
     public final void testGetDigestsEmpty() throws Exception
     {
-        Path emptyFile = testDir.resolve("emptyfile.txt");
+        final Path emptyFile = this.testDir.resolve("emptyfile.txt");
         Files.createFile(emptyFile);
         
         Assert.assertTrue(Files.exists(emptyFile));
         Assert.assertEquals(0, Files.size(emptyFile));
         
-        ConcurrentMap<Path, ConcurrentMap<String, String>> digests =
+        final ConcurrentMap<Path, ConcurrentMap<Algorithm, String>> digests =
                 PoddDigestUtils.getDigests(Arrays.asList(emptyFile));
         
         Assert.assertEquals(1, digests.size());
         
         Assert.assertTrue(digests.containsKey(emptyFile));
         
-        ConcurrentMap<String, String> emptyFileDigests = digests.get(emptyFile);
+        final ConcurrentMap<Algorithm, String> emptyFileDigests = digests.get(emptyFile);
         
         Assert.assertEquals(2, emptyFileDigests.size());
         
-        Assert.assertTrue(emptyFileDigests.containsKey("MD5"));
-        Assert.assertTrue(emptyFileDigests.containsKey("SHA-1"));
+        Assert.assertTrue(emptyFileDigests.containsKey(PoddDigestUtils.Algorithm.MD5));
+        Assert.assertTrue(emptyFileDigests.containsKey(PoddDigestUtils.Algorithm.SHA1));
         
-        Assert.assertEquals("d41d8cd98f00b204e9800998ecf8427e", emptyFileDigests.get("MD5"));
-        Assert.assertEquals("da39a3ee5e6b4b0d3255bfef95601890afd80709", emptyFileDigests.get("SHA-1"));
+        Assert.assertEquals("d41d8cd98f00b204e9800998ecf8427e", emptyFileDigests.get(PoddDigestUtils.Algorithm.MD5));
+        Assert.assertEquals("da39a3ee5e6b4b0d3255bfef95601890afd80709",
+                emptyFileDigests.get(PoddDigestUtils.Algorithm.SHA1));
     }
     
     @Test
     public final void testGetDigestsShort() throws Exception
     {
-        Path shortFile = testDir.resolve("shortfile.txt");
+        final Path shortFile = this.testDir.resolve("shortfile.txt");
         Files.copy(
                 new ByteArrayInputStream("The quick brown fox jumps over the lazy dog".getBytes(StandardCharsets.UTF_8)),
                 shortFile);
@@ -92,22 +91,23 @@ public class PoddDigestUtilsTest
         Assert.assertTrue(Files.exists(shortFile));
         Assert.assertEquals(43, Files.size(shortFile));
         
-        ConcurrentMap<Path, ConcurrentMap<String, String>> digests =
+        final ConcurrentMap<Path, ConcurrentMap<Algorithm, String>> digests =
                 PoddDigestUtils.getDigests(Arrays.asList(shortFile));
         
         Assert.assertEquals(1, digests.size());
         
         Assert.assertTrue(digests.containsKey(shortFile));
         
-        ConcurrentMap<String, String> shortFileDifests = digests.get(shortFile);
+        final ConcurrentMap<Algorithm, String> shortFileDifests = digests.get(shortFile);
         
         Assert.assertEquals(2, shortFileDifests.size());
         
-        Assert.assertTrue(shortFileDifests.containsKey("MD5"));
-        Assert.assertTrue(shortFileDifests.containsKey("SHA-1"));
+        Assert.assertTrue(shortFileDifests.containsKey(PoddDigestUtils.Algorithm.MD5));
+        Assert.assertTrue(shortFileDifests.containsKey(PoddDigestUtils.Algorithm.SHA1));
         
-        Assert.assertEquals("9e107d9d372bb6826bd81d3542a419d6", shortFileDifests.get("MD5"));
-        Assert.assertEquals("2fd4e1c67a2d28fced849ee1bb76e7391b93eb12", shortFileDifests.get("SHA-1"));
+        Assert.assertEquals("9e107d9d372bb6826bd81d3542a419d6", shortFileDifests.get(PoddDigestUtils.Algorithm.MD5));
+        Assert.assertEquals("2fd4e1c67a2d28fced849ee1bb76e7391b93eb12",
+                shortFileDifests.get(PoddDigestUtils.Algorithm.SHA1));
     }
     
 }
